3fe3ed3a36a664ff26014458b68c1951d0d70f73
ynakano1127 <ynakano1127@gmail.com>
2018-06-14 11:43:21 +0900

    [add]ファイルを追加

--- server/api/index.js
+++ server/api/index.js
@@ -1,10 +1,12 @@
 import { Router } from 'express'
 
 import users from './users'
+import arrangement from './arrangement'
 
 const router = Router()
 
 // Add USERS Routes
 router.use(users)
+router.use(arrangement)
 
 export default router
