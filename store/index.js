61ac3e69e387f12670084b9aad8d56cd8d2ed356
ynakano1127 <ynakano1127@gmail.com>
2018-06-16 16:33:04 +0900

    [add]動きをつけた

--- store/index.js
+++ store/index.js
@@ -2,13 +2,26 @@ export const state = () => ({
   papers: [{
     imgUrl: '/test-page-001.jpg',
     x: 0,
-    y: 0
-  }]
+    y: 0,
+    isSelected: false
+  }, {
+    imgUrl: '/test-page-001.jpg',
+    x: 0,
+    y: 400,
+    isSelected: false
+  }
+  ]
 })
 
 export const mutations = {
   move (state, { paperId, x, y }) {
     state.papers[paperId].x = x
     state.papers[paperId].y = y
+  },
+  selectCard (state, {paperId}) {
+    for (let i in state.papers) {
+      state.papers[i].isSelected = false
+    }
+    state.papers[paperId].isSelected = true
   }
 }
