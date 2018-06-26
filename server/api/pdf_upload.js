1154242f5409d4fe8a4cc1f1c93b60f6cf1047bc
ynakano1127 <ynakano1127@gmail.com>
2018-06-26 15:36:42 +0900

    [add]docxを受け付けるようにした

--- server/api/pdf_upload.js
+++ server/api/pdf_upload.js
@@ -2,24 +2,35 @@ import { Router } from 'express'
 import app from 'express'
 
 import multer from 'multer'
+import { extname } from 'path';
 
 const router = Router()
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, '.pdf/')
+    //拡張子(.で切って一番最後の要素)をみる
+    switch(file.originalname.split('.').pop()){
+      case 'pdf':
+        cb(null, '.document/pdf')
+        break;
+      case 'docx':
+      case 'doc':
+        cb(null, '.document/docx')
+        break
+      default:
+        cb(null, '.document/unknown')
+    }
   },
   filename: (req, file, cb) => {
-    cb(null, file.fieldname + '-' + Date.now() + '.pdf')
+    cb(null, Date.now() + '.' + file.originalname.split('.').pop())
   }
 })
 
 const upload = multer({ storage: storage })
 
-router.post('/pdf', upload.single('pdf'), (req, res, next) => {
+router.post('/upload-file', upload.single('file'), (req, res, next) => {
   console.log(req.body)
   res.send(req.body)
-  //grade , cls , coordinate
 })
 
 export default router
