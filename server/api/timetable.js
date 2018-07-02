363bb7b2e2bf47cc74c86c227e656a22d59b56b1
KitsuKitsu <akira1410.k@gmail.com>
2018-07-02 23:51:04 +0900

    [fix]timetableのjsonの仕様を決定

--- server/api/timetable.js
+++ server/api/timetable.js
@@ -1,7 +1,12 @@
 import { Router } from 'express'
+import 'puppeteer'
 import 'date-utils'
 
 const router = Router()
+
+router.get('/gettable', function (req, res, next) {
+  res.json()
+})
 
 router.get('/settable', function (req, res, next) {
   var dt = new Date()
