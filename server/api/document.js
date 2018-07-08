d733033aebab430a3311563e58eeb893ca265e5d
YutoTamura <s17130@tokyo.kosen-ac.jp>
2018-07-08 15:21:58 +0900

    [add] apiのそれの雛形

---
+++ server/api/document.js
@@ -0,0 +1,18 @@
+import { Router } from 'express'
+
+const router = Router()
+
+/* GET users listing. */
+router.get('/documents', (req,res,next) =>{
+
+})
+router.post('/document',(req,res,next) => {
+
+})
+router.post('/class', (req, res, next) => {
+
+})
+router.get('/class', (req, res, next) => {
+
+})
+export default router
