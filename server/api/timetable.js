77974e7b8dcb149846234b46ad3a61d38729e8e3
KitsuKitsu <akira1410.k@gmail.com>
2018-07-09 18:13:04 +0900

    [add]とりあえず平日のjsonファイルを取得できるようにした

--- server/api/timetable.js
+++ server/api/timetable.js
@@ -1,18 +1,31 @@
 import { Router } from 'express'
-import 'puppeteer'
 import 'date-utils'
+import {readdirSync} from 'fs'
+import {resolve} from 'path'
 
 const router = Router()
+const date = new Date()
 
 router.get('/gettable', function (req, res, next) {
   res.json()
 })
 
-router.get('/settable', function (req, res, next) {
-  var dt = new Date()
-  var time = dt.toFormat('YYYYMMDDHH24MISS')
-  console.log(time)
-  res.json(time)
+router.get('/settable', function (dirPath, res) {
+  dirPath = resolve('assets', '../assets')
+  var filename = readdirSync(dirPath)
+  var dt = date.toFormat('DDDD')
+  var list = []
+
+  if (dt === 'Sunday') {
+  } else if (dt === 'Saturday') {
+  } else {
+    for (var i = 0; i < filename.length; i++) {
+      if (filename[i].match(/weekdays/)) {
+        list.push(filename[i])
+      }
+    }
+  }
+  res.json(list)
 })
 
 export default router
