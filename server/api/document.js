3f897f9a1a97fe4c6d579941058e1c0a2ad47ee4
YutoTamura <s17130@tokyo.kosen-ac.jp>
2018-07-09 19:15:38 +0900

    とりあえず

--- server/api/document.js
+++ server/api/document.js
@@ -1,18 +1,8 @@
 import { Router } from 'express'
-
+const models = require("../../models")
 const router = Router()
 
-/* GET users listing. */
-router.get('/documents', (req,res,next) =>{
-
-})
-router.post('/document',(req,res,next) => {
-
-})
-router.post('/class', (req, res, next) => {
-
-})
 router.get('/class', (req, res, next) => {
-
+  res.send("HELLO")
 })
 export default router
--- server/api/index.js
+++ server/api/index.js
@@ -2,11 +2,13 @@ import { Router } from 'express'
 
 import users from './users'
 import upload_file from './upload-file'
+import doc from './document'
 
 const router = Router()
 
 // Add USERS Routes
 router.use(users)
 router.use(upload_file)
+router.use(doc)
 
 export default router
