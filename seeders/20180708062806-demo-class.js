f25a1572c254ae499524accc447608a2fc2ae1e7
YutoTamura <s17130@tokyo.kosen-ac.jp>
2018-07-09 16:26:49 +0900

    [add] seed データ

---
+++ seeders/20180708062806-demo-class.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+      var doc = {
+        "path":"test-page-001.jpg",
+        "x":0,
+        "y":0
+      }
+      return queryInterface.bulkInsert('classes', [{
+        classname: 'demo-class',
+        document: [JSON.stringify(doc),JSON.stringify(doc)],
+        createdAt:new Date(),
+        updatedAt: new Date(),
+      }], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+      return queryInterface.bulkDelete('classes', null, {});
+  }
+};
