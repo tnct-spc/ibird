300f4073af2b73e5df4f35191039c7418f641a18
YutoTamura <s17130@tokyo.kosen-ac.jp>
2018-07-09 16:20:20 +0900

    [add] 新しいdocumentcclimun TEXT(型)

---
+++ migrations/20180709071704-addNewDocumentColumn.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.addColumn(
+      'classes',
+      'document',
+      Sequelize.ARRAY(Sequelize.TEXT)
+    );
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.removeColumn(
+      'classes',
+      'document',
+    );
+  },
+};
