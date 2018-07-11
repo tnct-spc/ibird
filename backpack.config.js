d9f904e920715b21c84e8935a5a7ce906b93b546
YutoTamura <s17130@tokyo.kosen-ac.jp>
2018-07-11 23:04:26 +0900

    [fix] modelをimportしてもエラーがでなくなるように

--- backpack.config.js
+++ backpack.config.js
@@ -1,6 +1,11 @@
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
 module.exports = {
   webpack: (config, options, webpack) => {
     config.entry.main = './server/index.js'
+    config.plugins = [
+        new CopyWebpackPlugin([{from: './server/models/', to:'./server/models/'}], {ignore: ['index.js']})
+    ]
     return config
   }
 }
--- server/api/document.js
+++ server/api/document.js
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 const router = Router()
+import models from '../models'
 
 router.get('/class', (req, res, next) => {
   res.send("HELLO")
