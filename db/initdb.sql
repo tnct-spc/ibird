7617673e2004653e1137a15c8dc639ab8c261255
ynakano1127 <ynakano1127@gmail.com>
2018-07-16 18:03:30 +0900

    [fix]初期値を入れる

--- db/initdb.sql
+++ db/initdb.sql
@@ -1,9 +1,15 @@
+DROP TABLE IF EXISTS ibird;
 CREATE DATABASE ibird;
 
 \c ibird;
 
-CREATE TABLE clases (
+CREATE TABLE classes (
   id integer primary key,
   name text,
   douments json
 );
+
+INSERT INTO classes
+VALUES  (1, 'A', '{"a": "b", "c": "d"}'),
+        (2, 'B', '{"a": "b", "c": "d"}'),
+        (3, 'C', '{"a": "b", "c": "d"}');
