a8cf3283254c00e2899f4e723cb8bcbce4ededc9
ynakano1127 <ynakano1127@gmail.com>
2018-07-16 18:51:57 +0900

    [add]データベースと接続確認できた

--- server/api/classes.js
+++ server/api/classes.js
@@ -1,9 +1,31 @@
 import { Router } from 'express'
+import Sequelize from 'sequelize'
 
 const router = Router()
 
-router.get('/class', (req, res, next) => {
-    console.log("HelloWorld")
+const sequelize = new Sequelize('ibird', 'postgres', 'password',{
+    host: 'postgres',
+    dialect: 'postgres',
+})
+// const classes = sequelize.define('classes', {
+//     name: {
+//       type: Sequelize.STRING
+//     },
+//     douments: {
+//         type: Sequelize.JSON
+//     }
+//   },{
+//       timestamps: true
+//   });
+
+router.get('/classes', (req, res, next) => {
+    sequelize.authenticate()
+        .then(() => {
+            console.log('Connection has been established successfully.');
+        })
+        .catch(err => {
+            console.error('Unable to connect to the database:', err);
+        });
     res.sendStatus(200)
 })
 
