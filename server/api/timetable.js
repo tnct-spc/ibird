95ff5884ce24a9619215981b5130460c6a0c5b9e
KitsuKitsu <akira1410.k@gmail.com>
2018-07-17 18:32:30 +0900

    [add]表示可能なjsonのファイル名+その情報を返すAPIを書いた

--- server/api/timetable.js
+++ server/api/timetable.js
@@ -1,43 +1,60 @@
 import { Router } from 'express'
+import 'body-parser'
 import 'date-utils'
-import {readdirSync} from 'fs'
+import {readdirSync, readFileSync} from 'fs'
 import {resolve} from 'path'
 
 const router = Router()
 const date = new Date()
+var dirPath = resolve('assets', '../assets') // assetsのパスを取得
 var list = []
 
-router.get('/gettable', function (req, res, next) {
+// 駅のタイムテーブルを取得してJSONを生成するAPI
+router.get('/creatTable', function (req, res) {
+  res.send('設定ファイルの作成に成功しました')
+})
+
+// 表示できるJSONファイルの情報を返すAPI
+router.get('/getFileList', function (req, res) {
+  var fileInfo = JSON.parse(readFileSync(dirPath + '/filename.json', 'utf8'))
+  var ans = []
+  list.length = 0
+  getlist()
+  for (var i = 0; i < list.length; i++) {
+    for (var j = 0; j < fileInfo.length; j++) {
+      if (list[i] === fileInfo[j].name) {
+        ans.push(fileInfo[j])
+      }
+    }
+  }
+  res.json(ans)
+})
+
+// 最終的に表示するJSONのタイムテーブルを返すAPI
+router.get('/sendTable', function (req, res) {
   res.json()
 })
 
-router.get('/settable', function (req, res, next) {
-  list.length = 0
-  getlist()
-  res.json(list)
-})
-
 function getlist () {
-  var dirPath = resolve('assets', '../assets')
-  var filename = readdirSync(dirPath)
+  var filelist = readdirSync(dirPath) // 全ファイルのリストを生成
   var dt = date.toFormat('DDDD')
 
   if (dt === 'Sunday') {
-    for (var i = 0; i < filename.length; i++) {
-      if (filename[i].match(/holidays/)) {
-        list.push(filename[i])
+    for (var i = 0; i < filelist.length; i++) {
+      if (filelist[i].match(/holidays/)) {
+        list.push(filelist[i])
       }
     }
   } else if (dt === 'Saturday') {
-    for (var j = 0; j < filename.length; j++) {
-      if (filename[j].match(/weekenddays/)) {
-        list.push(filename[j])
+    for (var j = 0; j < filelist.length; j++) {
+      if (filelist[j].match(/weekenddays/)) {
+        list.push(filelist[j])
       }
     }
   } else {
-    for (var k = 0; k < filename.length; k++) {
-      if (filename[k].match(/weekdays/)) {
-        list.push(filename[k])
+    for (var k = 0; k < filelist.length; k++) {
+      if (filelist[k].match(/weekdays/)) {
+        list.push(filelist[k])
       }
     }
   }
