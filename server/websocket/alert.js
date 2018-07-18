d0858378767d27bdad01d58f765561851a769806
OguraNozomu <s17040@tokyo.kosen-ac.jp>
2018-07-18 17:49:48 +0900

    [add]ﾅﾝﾄｶｶﾝﾄｶ

--- server/websocket/alert.js
+++ server/websocket/alert.js
@@ -7,10 +7,24 @@ expressWs(router)
 const receivers = []
 router.ws('/alert',function(ws, req){
   const idx = receivers.push(ws)-1
+
+  let url='http://svir.jp/eew/data.json'
+  let request=new XMLHttpRequest()
+  request.open('GET',url)
+
+  request.onreadystatechange=function(){
+    if(request.readyState!=4){
+    }else if(request.status!=200){
+      console.log("200")
+    }else{
+      let result=request.responseText;
+    }
+  }
+
   ws.on('message', msg => {
     console.log('alert added: ' + msg)
     receivers.forEach((receiver,receiverId)=>{
-      if(receiverId!==idx){receiver.send(msg)}
+      if(receiverId!==idx){}
     })
   })
   ws.on('close', ()=>{
