bc25fa93691ca211faa7cbe11413a353fd0f8de9
KitsuKitsu <akira1410.k@gmail.com>
2018-07-19 17:24:07 +0900

    [fix]最終的に表示するJSONのタイムテーブルを返すAPIを動くようにした

--- server/api/timetable.js
+++ server/api/timetable.js
@@ -1,12 +1,11 @@
 import { Router } from 'express'
-import 'body-parser'
 import 'date-utils'
-import {readdirSync, readFileSync} from 'fs'
-import {resolve} from 'path'
+import { readdirSync, readFileSync } from 'fs'
+import { resolve } from 'path'
 
 const router = Router()
 const date = new Date()
-var dirPath = resolve('assets', '../assets') // assetsのパスを取得
+var dirPath = resolve('assets', '../assets') // assetsのパスを設定
 var list = []
 
 // 駅のタイムテーブルを取得してJSONを生成するAPI
@@ -19,43 +18,47 @@ router.get('/getFileList', function (req, res) {
   var fileInfo = JSON.parse(readFileSync(dirPath + '/filename.json', 'utf8'))
   var ans = []
   list.length = 0
-  getlist()
-  for (var i = 0; i < list.length; i++) {
+  getlist(ans)
+  for (var i = 0; i < ans.length; i++) {
     for (var j = 0; j < fileInfo.length; j++) {
-      if (list[i] === fileInfo[j].name) {
-        ans.push(fileInfo[j])
+      if (ans[i] === fileInfo[j].name) {
+        list.push(fileInfo[j])
       }
     }
   }
-  res.json(ans)
+  res.json(list)
 })
 
 // 最終的に表示するJSONのタイムテーブルを返すAPI
 router.get('/sendTable', function (req, res) {
-  console.log(req.query)
-  res.json(req.query)
+  list.length = 0
+  for (var i = 0; i < req.query.file.length; i++) {
+    list.push(JSON.parse(readFileSync(dirPath + '/' + req.query.file[i], 'utf8')))
+  }
+  console.log(list)
+  res.json(list)
 })
 
-function getlist () {
+function getlist (todayList) {
   var filelist = readdirSync(dirPath) // 全ファイルのリストを生成
   var dt = date.toFormat('DDDD')
 
   if (dt === 'Sunday') {
     for (var i = 0; i < filelist.length; i++) {
       if (filelist[i].match(/holidays/)) {
-        list.push(filelist[i])
+        todayList.push(filelist[i])
       }
     }
   } else if (dt === 'Saturday') {
     for (var j = 0; j < filelist.length; j++) {
       if (filelist[j].match(/weekenddays/)) {
-        list.push(filelist[j])
+        todayList.push(filelist[j])
       }
     }
   } else {
     for (var k = 0; k < filelist.length; k++) {
       if (filelist[k].match(/weekdays/)) {
-        list.push(filelist[k])
+        todayList.push(filelist[k])
       }
     }
   }
