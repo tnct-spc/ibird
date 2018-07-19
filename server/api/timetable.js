bb7663f48e0e7e4d676fdc42cf72deb8fe21a1e5
KitsuKitsu <akira1410.k@gmail.com>
2018-07-19 17:59:52 +0900

    [fix]ゴミみたいな変数名を修正

--- server/api/timetable.js
+++ server/api/timetable.js
@@ -16,12 +16,12 @@ router.get('/creatTable', function (req, res) {
 // 表示できるJSONファイルの情報を返すAPI
 router.get('/getFileList', function (req, res) {
   var fileInfo = JSON.parse(readFileSync(dirPath + '/filename.json', 'utf8'))
-  var ans = []
+  var todayFile = []
   list.length = 0
-  getlist(ans)
-  for (var i = 0; i < ans.length; i++) {
+  getlist(todayFile)
+  for (var i = 0; i < todayFile.length; i++) {
     for (var j = 0; j < fileInfo.length; j++) {
-      if (ans[i] === fileInfo[j].name) {
+      if (todayFile[i] === fileInfo[j].name) {
         list.push(fileInfo[j])
       }
     }
@@ -35,30 +35,29 @@ router.get('/sendTable', function (req, res) {
   for (var i = 0; i < req.query.file.length; i++) {
     list.push(JSON.parse(readFileSync(dirPath + '/' + req.query.file[i], 'utf8')))
   }
-  console.log(list)
   res.json(list)
 })
 
-function getlist (todayList) {
+function getlist (todayFileList) {
   var filelist = readdirSync(dirPath) // 全ファイルのリストを生成
   var dt = date.toFormat('DDDD')
 
   if (dt === 'Sunday') {
     for (var i = 0; i < filelist.length; i++) {
       if (filelist[i].match(/holidays/)) {
-        todayList.push(filelist[i])
+        todayFileList.push(filelist[i])
       }
     }
   } else if (dt === 'Saturday') {
     for (var j = 0; j < filelist.length; j++) {
       if (filelist[j].match(/weekenddays/)) {
-        todayList.push(filelist[j])
+        todayFileList.push(filelist[j])
       }
     }
   } else {
     for (var k = 0; k < filelist.length; k++) {
       if (filelist[k].match(/weekdays/)) {
-        todayList.push(filelist[k])
+        todayFileList.push(filelist[k])
       }
     }
   }
