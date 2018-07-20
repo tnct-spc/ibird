d64d07a8140cca1a5b8ab90cecfc6db52cae7e11
SuzukiSoichiro <s18094@tokyo.kosen-ac.jp>
2018-07-20 17:16:12 +0900

    [add]Localに画像をぶん投げる

--- server/api/weather_forecast.js
+++ server/api/weather_forecast.js
@@ -5,7 +5,7 @@ const router = Router()
 const fetch = require('node-fetch')
 
 router.get("/yolp", function (req, res, next) {
-    const yahooAppId = "dj0zaiZpPXVZaDlrczVieXNFYSZzPWNvbnN1bWVyc2VjcmV0Jng9ZGE-" // Yahooで登録してこのIDを取得しないといけない。それをここに書く。
+    const yahooAppId = "dj0zaiZpPXVZaDlrczVieXNFYSZzPWNvbnN1bWVyc2VjcmV0Jng9ZGE-"
     const lat = req.query.lat
     const lon = req.query.lon
     const z = req.query.z
@@ -16,20 +16,22 @@ router.get("/yolp", function (req, res, next) {
     console.log(lat)
     console.log(lon)
     console.log(z)
+    console.log(width)
+    console.log(height)
+    console.log(yyyymmdd)
     
     const url = "https://map.yahooapis.jp/map/V1/static?appid=" + yahooAppId + "&lat=" + lat + "&lon=" + lon + "&z=" + z + "&width=" + width + "&height=" + height + "&mode=map&overlay=type:rainfall|date:" + yyyymmdd + "1200|datelabel:on&output=jpeg"
 
     console.log(url)
-     fetch(url)
-         // 結果は画像のバイナリデータとして返ってくる。
-         .then(data => data.buffer()) // ここでBuffer型オブジェクトにする。※これはブラウザであるようなfetchではなくnode-fetchだけの拡張機能。
-         .then(data => {
-             res.writeHead(200, { 'Content-Type': 'image/jpeg' }); // レスポンスとしてjpeg送るぞ！という情報をセット
-             res.write(data); // レスポンスに値書き込む。
-             res.send(); // レスポンス送る
-         })
-         .catch(err => console.error(err)); // Promiseのチェーンでエラーが起きたときにそれをログとして吐き出している。
-    res.sendStatus(200)
-});
+
+    fetch(url)
+    .then(data => data.buffer())
+    .then(data => {
+        res.writeHead(200, { 'Content-Type': 'image/jpeg' })
+        res.write(data)
+        res.send()
+    })
+    .catch(err => console.error(err))
+})
 
 export default router
