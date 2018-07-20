512872a88b1bb4b23f3243881033331f973820e6
SuzukiSoichiro <s18094@tokyo.kosen-ac.jp>
2018-07-20 15:26:49 +0900

    [add]リクエストを受け取れるようになった！

--- server/api/weather_forecast.js
+++ server/api/weather_forecast.js
@@ -2,26 +2,34 @@ import { Router } from 'express'
 
 const router = Router()
 
-router.get("/yolp/:yyyymmdd", function (req, res, next) {
-    const yahooAppId = req.params.yahooAppId; // Yahooで登録してこのIDを取得しないといけない。それをここに書く。これは僕のIDなので、試す目的以外で使わないでね！
-    const lat = req.params.lat;
-    const lon = req.params.lon;
-    const z = req.params.z;
-    const width = req.params.width;
-    const height = req.params.height;
-    const yyyymmdd = req.params.yyyymmdd
-    
-    const url = "https://map.yahooapis.jp/map/V1/static?appid=" + yahooAppId + "&lat=" + lat + "&lon=" + lon + "&z=11&width=300&height=200&mode=map&overlay=type:rainfall|date:" + yyyymmdd + "1200|datelabel:on&output=jpeg"
+const fetch = require('node-fetch')
 
-    fetch(url)
-        // 結果は画像のバイナリデータとして返ってくる。
-        .then(data => data.buffer()) // ここでBuffer型オブジェクトにする。※これはブラウザであるようなfetchではなくnode-fetchだけの拡張機能。
-        .then(data => {
-            res.writeHead(200, { 'Content-Type': 'image/jpeg' }); // レスポンスとしてjpeg送るぞ！という情報をセット
-            res.write(data); // レスポンスに値書き込む。
-            res.send(); // レスポンス送る
-        })
-        .catch(err => console.error(err)); // Promiseのチェーンでエラーが起きたときにそれをログとして吐き出している。
+router.get("/yolp", function (req, res, next) {
+    const yahooAppId = "dj0zaiZpPXVZaDlrczVieXNFYSZzPWNvbnN1bWVyc2VjcmV0Jng9ZGE-" // Yahooで登録してこのIDを取得しないといけない。それをここに書く。
+    const lat = req.query.lat
+    const lon = req.query.lon
+    const z = req.query.z
+    const width = req.query.width
+    const height = req.query.height
+    const yyyymmdd = req.query.yyyymmdd
+    
+    console.log(lat)
+    console.log(lon)
+    console.log(z)
+    
+    const url = "https://map.yahooapis.jp/map/V1/static?appid=" + yahooAppId + "&lat=" + lat + "&lon=" + lon + "&z=" + z + "&width=" + width + "&height=" + height + "&mode=map&overlay=type:rainfall|date:" + yyyymmdd + "1200|datelabel:on&output=jpeg"
+
+    console.log(url)
+     fetch(url)
+         // 結果は画像のバイナリデータとして返ってくる。
+         .then(data => data.buffer()) // ここでBuffer型オブジェクトにする。※これはブラウザであるようなfetchではなくnode-fetchだけの拡張機能。
+         .then(data => {
+             res.writeHead(200, { 'Content-Type': 'image/jpeg' }); // レスポンスとしてjpeg送るぞ！という情報をセット
+             res.write(data); // レスポンスに値書き込む。
+             res.send(); // レスポンス送る
+         })
+         .catch(err => console.error(err)); // Promiseのチェーンでエラーが起きたときにそれをログとして吐き出している。
+    res.sendStatus(200)
 });
 
 export default router
