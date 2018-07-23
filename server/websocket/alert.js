42cd3ad2a74e4152206e384d3929719ae19072ed
OguraNozomu <s17040@tokyo.kosen-ac.jp>
2018-07-23 18:19:26 +0900

    [update]httpリクエスト

--- server/websocket/alert.js
+++ server/websocket/alert.js
@@ -1,35 +1,31 @@
 import { Router } from 'express'
 import expressWs from 'express-ws'
-
 const router = Router()
 expressWs(router)
 
-const receivers = []
-router.ws('/alert',function(ws, req){
-  const idx = receivers.push(ws)-1
+let url = "http://svir.jp/eew/data.json"
+let result,c=0
+function {
+  setInterval("request(url)",5000)
+}
 
-  let url='http://svir.jp/eew/data.json'
-  let request=new XMLHttpRequest()
-  request.open('GET',url)
+function request(url) {
+  let request = new XMLHttpRequest()
+  request.open("GET",url)
+  request.onreadystatechange = function(){httpresponse(request)}
+  request.send(null)
+}
 
-  request.onreadystatechange=function(){
-    if(request.readyState!=4){
+function httpresponse(request){
+  if(request.readyState!=4){
     }else if(request.status!=200){
       console.log("200")
     }else{
-      let result=request.responseText;
+      let confirmation = request.responseText
+      c++
+      if(c!=0&&confirmation!=result){
+        result = confirmation
+      }
     }
   }
-
-  ws.on('message', msg => {
-    console.log('alert added: ' + msg)
-    receivers.forEach((receiver,receiverId)=>{
-      if(receiverId!==idx){}
-    })
-  })
-  ws.on('close', ()=>{
-    receivers.splice(idx,1)
-  })
-})
-
-export default router
+}
