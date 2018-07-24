377eab00d5478eba1fea82ed47a1f80d1321f634
SuzukiSoichiro <s18094@tokyo.kosen-ac.jp>
2018-07-24 13:46:04 +0900

    [fix]usersとweatherをもういっかいrouterにしました

--- server/api/index.js
+++ server/api/index.js
@@ -1,10 +1,12 @@
 import { Router } from 'express'
 
 import users from './users'
+import weather from './weather_forecast'
 
 const router = Router()
 
 // Add USERS Routes
 router.use(users)
+router.use(weather)
 
 export default router
