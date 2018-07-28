43dab9b25d54b169d0c7d2937520c251ccd99040
ynakano1127 <ynakano1127@gmail.com>
2018-07-28 21:08:58 +0900

    [add]URLの確認作業

--- pages/bb/_hr.vue
+++ pages/bb/_hr.vue
@@ -1,15 +1,38 @@
 <template>
   <section>
     <classes/>
+    <p>{{classes}}</p>
   </section>
 </template>
 
 <script>
-import classes from '~/components/Classes.vue'
-export default{
+// import classes from '~/components/Classes.vue'
+import axios from 'axios'
+
+export default {
+  data () {
+    return {
+      classes: {}
+    }
+  },
+  asyncData ({ params, error }) {
+    return axios.get('http://' +process.env.mainUrl + '/api/classes-list').then(res =>{
+      const classlist = res.data
+      //ここでurlの確認
+      var eflag = true
+      classlist.forEach(c => {
+        if(String(c.classid) === params.hr) eflag = false
+      });
+      if(eflag) throw new URIError("URIちがうよ");
+
+      return {classes: classlist}
+    }).catch(err =>{
+      error({ statusCode: 404, message: 'ページが見つかりません' })
+    })
+  },
   components:{
-  classes
- },
+    // classes
+  },
 }
 </script>
 
