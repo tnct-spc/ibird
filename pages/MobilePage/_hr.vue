900a1f161f51ee6a9e6a2581909c1f8b742b9c9c
OguraNozomu <s17040@tokyo.kosen-ac.jp>
2018-08-03 20:33:01 +0900

    [add]/MobilePage/_hr.vue追加＊仮UI作った

---
+++ pages/MobilePage/_hr.vue
@@ -0,0 +1,35 @@
+<template>
+  <table id="Documents-index" class="table table-striped">
+    <thead>
+      <tr>
+        <th>Documents</th>
+        <th>{{hoge}}</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="document in documents">
+        <td>{{document.name}}</td>
+        <td>
+          <div class="btn btn-danger" v-on:click="seeDocument(document.id)">See</div>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+<script>
+export default{
+  data(){
+    return{
+      hoge:'hello,world!',
+      documents:[
+        {id:1,name:'sample01'},
+        {id:2,name:'sample02'},
+      ],
+      newDocument:'',
+    }
+  },
+
+}
+</script>
+<style>
+</style>
