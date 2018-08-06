9a19edb3376ed0262d520f6cd751af42d1d779c6
KitsuKitsu <akira1410.k@gmail.com>
2018-08-06 09:45:06 +0900

    [fix]非同期的にレスポンスするようにした

--- server/api/timetable.js
+++ server/api/timetable.js
@@ -9,18 +9,98 @@ var dirPath = resolve('.timetable', '../.timetable') // jsonのパスを設定
 var list = []
 
 // 駅のタイムテーブルを取得してJSONを生成するAPI
-router.get('/createtable', function (req, res) {
-  var scrapeData
-  fetchYahoo(scrapeData, req.query.url, createJsonFile)
-  res.send('ok')
+router.get('/createtable', (req, res) => {
+  var fullTimetableData = JSON.parse('{}')
+  fetch(req.query.url)
+    .then(function (result) {
+      var $ = result.$
+
+      // 駅名、行き先、何線かを取得
+      var anotherData = $('#cat-pass strong').text().split(' ')
+      fullTimetableData['station'] = anotherData[0]
+      fullTimetableData['direction'] = anotherData[2]
+      fullTimetableData['line'] = anotherData[1]
+
+      // 電車の車種と行き先のリストを作成
+      var kindList = JSON.parse('{}')
+      var goingList = JSON.parse('{}')
+      $('#timeNotice1 li').each(function () {
+        var kind = $(this).text().split('：')
+        kindList[kind[0]] = kind[1]
+      })
+      $('#timeNotice2 li').each(function () {
+        var going = $(this).text().split('：')
+        goingList[going[0]] = going[1]
+      })
+
+      // 電車が止まる時間帯だけのリストを作成
+      var ariveHour = []
+      $('.tblDiaDetail tr').each(function (i) {
+        var id = $(this).attr('id')
+        if (id) {
+          var idToNum = id.replace('hh_', '')
+          ariveHour.push(idToNum)
+        }
+      })
+
+      // 時刻表とその電車の車種、行き先を取得
+      var timetable = JSON.parse('{}')
+      for (var i = 1; i <= 24; i++) {
+        var fullHour = i.toString()
+        for (var j = 0; j < ariveHour.length; j++) {
+          if (ariveHour[j] === ariveHour[ariveHour.length - 1] && fullHour !== '24') {
+            timetable[fullHour] = []
+          }
+          if (fullHour === ariveHour[j]) {
+            var hourList = []
+            $('#hh_' + ariveHour[j] + ' .timeNumb').each(function () {
+              var trainData = JSON.parse('{}')
+              trainData['min'] = ($(this).find('dt').text())
+              if ($(this).find('.trainType').length) {
+                var trainType = $(this).find('.trainType').text().replace('[', '')
+                  .replace(']', '')
+                trainData['kind'] = kindList[trainType]
+              } else {
+                trainData['kind'] = kindList.無印
+              }
+              if ($(this).find('.trainFor').length) {
+                var trainFor = $(this).find('.trainFor').text()
+                trainData['going'] = goingList[trainFor]
+              } else {
+                trainData['going'] = goingList.無印
+              }
+              hourList.push(trainData)
+            })
+            timetable[fullHour] = hourList
+            break
+          }
+        }
+      }
+      fullTimetableData['timetable'] = timetable
+      console.log('createjson')
+    })
+    .then(function () {
+      createJsonFile(fullTimetableData, req.query.url)
+      console.log('createFile')
+    })
+
+    // エラー処理
+    .catch(function (error) {
+      console.log('Failed loadHTML: ' + error)
+      res.send('サーバー内でエラーが起きました')
+    })
+
+    // 成功
+    .finally(function () {
+      console.log('end')
+      res.send('ok')
+    })
 })
 
 // 表示できるJSONファイルの情報を返すAPI
-router.get('/getfilelist', function (req, res) {
+router.get('/getfilelist', (req, res) => {
   var fileInfo = JSON.parse(readFileSync(dirPath + '/filename.json', 'utf8'))
   list.length = 0
-  console.log(getTodayList())
-  console.log(getTodayList().length)
   for (var i = 0; i < getTodayList().length; i++) {
     for (var j = 0; j < fileInfo.length; j++) {
       if (getTodayList()[i] === fileInfo[j].name) {
@@ -44,7 +124,7 @@ router.get('/getfilelist', function (req, res) {
   }
   のようになります。
 */
-router.get('/sendtable', function (req, res) {
+router.get('/sendtable', (req, res) => {
   var timeTableData = JSON.parse(readFileSync(dirPath + '/' + req.query.file, 'utf8'))
   res.json(timeTableData)
 })
@@ -54,7 +134,7 @@ var getTodayList = () => {
   var todayFileList = []
   var filelist = readdirSync(dirPath) // 全ファイルのリストを生成
   filelist.some(function (name, i) {
-    if (name === 'filename.json') filelist.splice(i, 1)
+    if (name === 'filename.json') filelist.splice(i, 1) // リストからfilename.jsonを削除
   })
   var dt = new Date().getDay()
 
@@ -80,79 +160,6 @@ var getTodayList = () => {
   return todayFileList
 }
 
-// Yahooから時刻表と駅のデータをスクレイピングする関数
-function fetchYahoo (fullTimetableData, url, callback) {
-  fullTimetableData = JSON.parse('{}')
-  fetch(url).then(function (result) {
-    if (result.error) console.log('Failed fetchYahoo : ' + result.error)
-    var $ = result.$
-
-    // 駅名、行き先、何線かを取得
-    var anotherData = $('#cat-pass strong').text().split(' ')
-    fullTimetableData['station'] = anotherData[0]
-    fullTimetableData['direction'] = anotherData[2]
-    fullTimetableData['line'] = anotherData[1]
-
-    // 電車の車種と行き先のリストを作成
-    var kindList = JSON.parse('{}')
-    var goingList = JSON.parse('{}')
-    $('#timeNotice1 li').each(function () {
-      var kind = $(this).text().split('：')
-      kindList[kind[0]] = kind[1]
-    })
-    $('#timeNotice2 li').each(function () {
-      var going = $(this).text().split('：')
-      goingList[going[0]] = going[1]
-    })
-
-    // 電車が止まる時間帯だけのリストを作成
-    var ariveHour = []
-    $('.tblDiaDetail tr').each(function (i) {
-      var id = $(this).attr('id')
-      if (id) {
-        var idToNum = id.replace('hh_', '')
-        ariveHour.push(idToNum)
-      }
-    })
-
-    // 時刻表とその電車の車種、行き先を取得
-    var timetable = JSON.parse('{}')
-    for (var i = 1; i <= 24; i++) {
-      var fullHour = i.toString()
-      for (var j = 0; j < ariveHour.length; j++) {
-        if (ariveHour[j] === ariveHour[ariveHour.length - 1] && fullHour !== '24') {
-          timetable[fullHour] = []
-        }
-        if (fullHour === ariveHour[j]) {
-          var hourList = []
-          $('#hh_' + ariveHour[j] + ' .timeNumb').each(function () {
-            var trainData = JSON.parse('{}')
-            trainData['min'] = ($(this).find('dt').text())
-            if ($(this).find('.trainType').length) {
-              var trainType = $(this).find('.trainType').text().replace('[', '')
-                .replace(']', '')
-              trainData['kind'] = kindList[trainType]
-            } else {
-              trainData['kind'] = kindList.無印
-            }
-            if ($(this).find('.trainFor').length) {
-              var trainFor = $(this).find('.trainFor').text()
-              trainData['going'] = goingList[trainFor]
-            } else {
-              trainData['going'] = goingList.無印
-            }
-            hourList.push(trainData)
-          })
-          timetable[fullHour] = hourList
-          break
-        }
-      }
-    }
-    fullTimetableData['timetable'] = timetable
-    callback(fullTimetableData, url)
-  })
-}
-
 // fetchYahooのコールバックで呼び出すファイルを作成する関数
 function createJsonFile (jsonData, URL) {
   var filename
@@ -173,7 +180,7 @@ function createJsonFile (jsonData, URL) {
     }
   )
 
-  // filename.jsonを更新
+  /* // filename.jsonを更新
   var fileInfo = JSON.parse(readFileSync(dirPath + '/filename.json', 'utf8'))
   var jsonInfo = JSON.parse('{}')
   jsonInfo['name'] = filename
@@ -191,10 +198,10 @@ function createJsonFile (jsonData, URL) {
   writeJson(dirPath + '/filename.json', fileInfo, {spaces: 2},
     function (error) {
       if (error) {
-        console.log('Failed writeJson : ' + error)
+        console.log('Failed writeFilename : ' + error)
       }
     }
-  )
+  ) */
 }
 
 export default router
