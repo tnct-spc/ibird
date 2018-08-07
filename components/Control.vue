426c512ec23b41c24d4daf944296c042b0e374dd
siz <91zyutz@gmail.com>
2018-08-07 17:40:24 +0900

    [update]classidによるswitching

--- components/Control.vue
+++ components/Control.vue
@@ -1,7 +1,7 @@
 <template>
   <section>
-　<div id="overlay" @dragleave.prevent="onDragLeave" @dragover.prevent="onDragOver" @drop.prevent="onDrop">
-  管理者ページ
+　<div id="overlay" @dragleave.prevent="onDragLeave($event)" @dragover.prevent="onDragOver($event)" @drop.prevent="onDrop($event)">
+  {{text}}
 </div>
   </section>
 </template>
@@ -9,11 +9,19 @@
 import axios from 'axios'
 
 export default{
+  data:()=>{
+    return{
+      text:""
+    }
+  },
+  mounted(){
+    this.text="管理者ページ"
+  },
   methods: {
   onDragOver:(event) => {
     event.preventDefault()
     event.dataTransfer.dropEffect = "copy"
-    let overlay = document.getElementById("overlay")
+    const overlay = document.getElementById("overlay")
     overlay.classList.add("dropover")
   },
   onDragLeave:(event) => {
@@ -22,21 +30,20 @@ export default{
     event.preventDefault()
     event.dataTransfer.dropEffect="copy"
     overlay.classList.remove("dropover")
-    let files = event.dataTransfer.files[0]
+    const files = event.dataTransfer.files[0]
     if(!files.type.match('application/pdf')&&!files.type.match('application/vnd.*'))
     {
       overlay.innerHTML="ファイル形式に対応してません"
       return
     }
-    let formData = new FormData()
+    const formData = new FormData()
     formData.append( 'file', files )
     axios.post('api/upload-file',formData)
-    .then((response) => {
-        overlay.innerHTML="success"
+    .then((response)=>{
+      overlay.innerHTML="success"
     })
     .catch((error) => {
-        overlay.innerHTML="error"
-        console.log(error)
+      overlay.innerHTML=error
     })
   },
  }
--- components/ControlSelecter.vue
+++ components/ControlSelecter.vue
@@ -1,44 +1,107 @@
 <template>
   <div id="grid" style="margin-left:5px;margin-right:5px">
-    <button v-for="item in list" @click="switchingClass()" class="sitayose p">
+    <button v-for="item in grade" @click="switchingClass()" class="sitayose p">
       {{item.classes}}
     </button>
-    <button v-for="item in list2" @click="switchingClass()" class="p">
+    <button v-for="item in classes.list0" @click="switchingClassTable()" class="p"
       {{item.cource}}
     </button>
     <div style="text-align:center">
-      {{message}}
+      {{homeroom}}
     </div>
   </div>
 </template>
-
 <script>
+import axios from 'axios'
 export default{
-  props: ["v", "val"],
   data:()=>{
     return{
-    text:"",
-    message:"",
-    list:[
-      {id:1,classes:"1年"},
-      {id:2,classes:"2年"},
-      {id:3,classes:"3年"},
-      {id:4,classes:"4年"},
-      {id:5,classes:"5年"}
+    homeroom:"",
+    //dbからのclassidでやるべきだけどとりあえずの実装
+    grade:[
+      {id:"list1",classes:"1年"},
+      {id:"list2",classes:"2年"},
+      {id:"list3",classes:"3年"},
+      {id:"list4",classes:"4年"},
+      {id:"list5",classes:"5年"}
+    ],
+    classes:{
+    list0:[
+      {classid:"",cource:"学年を選択してください"},
+      {classid:"",cource:""},
+      {classid:"",cource:""},
+      {classid:"",cource:""},
+      {classid:"",cource:""}
+    ],
+    list1:[
+      {classid:"",cource:"1-1"},
+      {classid:"",cource:"1-2"},
+      {classid:"",cource:"1-3"},
+      {classid:"20180402",cource:"1-4"},
+      {classid:"20180401",cource:"1-5"}
     ],
     list2:[
-      {id:1,cource:"J"},
-      {id:2,cource:"D"},
-      {id:3,cource:"E"},
-      {id:4,cource:"M"},
-      {id:5,cource:"C"}
+      {classid:"20170401",cource:"2-J"},
+      {classid:"",cource:"2-D"},
+      {classid:"",cource:"2-E"},
+      {classid:"",cource:"2-M"},
+      {classid:"",cource:"2-C"}
     ],
+    list3:[
+      {classid:"20160401",cource:"3-J"},
+      {classid:"",cource:"3-D"},
+      {classid:"",cource:"3-E"},
+      {classid:"",cource:"3-M"},
+      {classid:"",cource:"3-C"}
+    ],
+    list4:[
+      {classid:"",cource:"4-J"},
+      {classid:"",cource:"4-D"},
+      {classid:"",cource:"4-E"},
+      {classid:"",cource:"4-M"},
+      {classid:"",cource:"4-C"}
+    ],
+    list5:[
+      {classid:"",cource:"5-J"},
+      {classid:"",cource:"5-D"},
+      {classid:"",cource:"5-E"},
+      {classid:"",cource:"5-M"},
+      {classid:"",cource:"5-C"}
+    ]
+    },
     v:0
     }
   },
   methods:{
     switchingClass(){
-     }
+      this.classes.list0.forEach((e,i)=>{
+        this.classes.list0[i]=this.classes.list2[i]
+      })
+      //this.homeroom="aaa"
+      //this.classes.list0.forEach((e,i)=>{
+        //if(element[0].id==="list1"){
+        //   this.classes.list0[i]=this.classes.list2[i]
+        //}
+        /*switch (element[i].id) {
+          case 'list1':this.classes.list0[i]=this.classes.list1[i]
+                       break
+          case "list2":this.classes.list0[i]=this.classes.list2[i]
+                       break
+          case "list3":this.classes.list0[i]=this.classes.list3[i]
+                       break
+          case "list4":this.classes.list0[i]=this.classes.list4[i]
+                       break
+          case "list5":this.classes.list0[i]=this.classes.list5[i]
+                       break
+        }*/
+      //})
+    },
+    switchingClassTable(){
+      this.list1[1].cource = "aa"
+      this.homeroom = this.items
+    },
+    refreshValue(element){
+    }
   },
 }
 </script>
