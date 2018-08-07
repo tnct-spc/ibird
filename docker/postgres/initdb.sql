f30339f5e4edb365fd36bcc5760a9823dd6b6d1e
YutoTamura <s17130@tokyo.kosen-ac.jp>
2018-08-07 16:06:44 +0900

    [add]tableを作成するmigration

--- docker/postgres/initdb.sql
+++ docker/postgres/initdb.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS ibird;
 CREATE DATABASE ibird;
 
 \c ibird;
-
+/*
 CREATE TABLE classes (
   classid integer primary key,
   name text,
@@ -12,3 +12,4 @@ CREATE TABLE classes (
 INSERT INTO classes VALUES  ('20160401', '3J',  '[{"docid": "20180401","x": 100,"y": 200},{"docid": "20180402","x": 100,"y": 200},{"docid": "20180403","x": 100,"y": 200}]');
 INSERT INTO classes VALUES  ('20170401', '2J',  '[{"docid": "20180402","x": 100,"y": 200},{"docid": "20180403","x": 100,"y": 200},{"docid": "20180404","x": 100,"y": 200}]');
 INSERT INTO classes VALUES  ('20180401', '1-5', '[{"docid": "20180403","x": 100,"y": 200},{"docid": "20180404","x": 100,"y": 200},{"docid": "20180405","x": 100,"y": 200}]');
+*/
---
+++ server/migrations/20180807065053-classes.js
@@ -0,0 +1,23 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('classes', { 
+      classid: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.TEXT
+      },
+      documents: {
+        type: Sequelize.JSON
+      },
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('classes');
+  }
+};
