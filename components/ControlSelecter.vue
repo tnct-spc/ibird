b89b9c7a22f6a56e2a45435f58bff4376ef48cfd
siz <91zyutz@gmail.com>
2018-08-16 15:45:01 +0900

    [fix]class-listからgridへの対応 layoutのfix

--- components/ControlSelecter.vue
+++ components/ControlSelecter.vue
@@ -1,14 +1,18 @@
 <template>
   <section>
-  <div id="grid" style="margin-left:5px;margin-right:5px">
+  <div style="margin-left:5px;margin-right:5px">
     <!-- 学年を表示する -->
-    <button v-for="(item, index) in obj" :key="index" @click="switchingClassTable(Object.keys(obj)[index-1])" class="sitayose p">
-      {{ Object.keys(obj)[index-1] }}　<!--indexが1始まりだったので-1している。要検討 -->
+    <div id="grid">
+    <button v-for="(item, index) in obj" @click="switchingClassTable(Object.keys(obj)[index-1])" class="sitayose p">
+      {{Object.keys(obj)[index-1]}}　<!--indexが1始まりだったので-1している。要検討 -->
     </button>
+    </div>
     <!-- 学年で選択されたクラスを表示する -->
+    <div id="grid2">
     <a :href=item.classid v-for="(item, index) in obj[year]" @click="switchingClass()" class="p">
       {{ item.course }}
     </a>
+    </div>
   </div>
  </section>
 </template>
@@ -23,17 +27,33 @@ export default {
     return{
       year: "", //現在選択中の学年
       obj: {},  //学年をKEYにclassidとcourseのオブジェクトの配列を持つ
+      grid:{}
     }
   },
-  created(){
-      this.classes.forEach(c => {
-        if(!this.obj[c.year]) Vue.set(this.obj, c.year, []) //最初の型を決める
+  mounted(){
+      const grid = document.getElementById("grid")
+      let grids = 0
+      this.classes.forEach((c)=> {
+        if(!this.obj[c.year]){
+          Vue.set(this.obj, c.year, [])
+          grids++
+        } //最初の型を決める
         this.obj[c.year].push({classid: c.classid, course: c.course})
-    })
+      })
+      grid.style.gridTemplateColumns="repeat("+String(grids)+", 1fr)"
+      for(let courseGrid in this.obj){
+        let grids2 = 0
+        this.obj[courseGrid].forEach((c)=> {
+          grids2++
+        })
+        Vue.set(this.grid, courseGrid, grids2)
+      }
   },
   methods:{
     switchingClassTable: function(newYear) { //新年じゃないよ
       this.year = newYear
+      const grid2 = document.getElementById("grid2")
+      grid2.style.gridTemplateColumns="repeat("+String(this.grid[this.year])+", 1fr)"
     },
     switchingClass: function(index) {}
   },
@@ -52,12 +72,16 @@ body{
   background-color: yellow;
 }
 .sitayose{
-  margin-top: 17%;
+  margin-top: 5px;
 }
 #grid{
   display: grid;
-  grid-template-rows:5% 5%;
-  grid-template-columns:1fr 1fr 1fr 1fr 1fr;
+  grid-template-rows:5%;
+  text-align: center;
+}
+#grid2{
+  display: grid;
+  grid-template-rows:5%;
   text-align: center;
 }
 </style>
