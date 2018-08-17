3bf0ced423e2542463781fe6af948f674161ec5e
YutoTamura <s17130@tokyo.kosen-ac.jp>
2018-08-17 16:35:57 +0900

    [add]マイグレーションファイルの作成

---
+++ server/migrations/20180817071815-adddocids.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return Promise.all([
+      queryInterface.addColumn(
+        'classes',
+        'docids',
+         Sequelize.ARRAY(Sequelize.INTEGER)
+      ),
+      queryInterface.removeColumn(
+        'classes',
+        'documents'
+      )
+    ])
+  },
+  down: (queryInterface, Sequelize) => {
+
+  }
+};
