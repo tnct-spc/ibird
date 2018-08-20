37497045641a1d0e43e332672901f42a50a07235
OguraNozomu <s17040@tokyo.kosen-ac.jp>
2018-08-20 15:08:08 +0900

    [update]リストを表示

--- pages/MobilePage/_hr.vue
+++ pages/MobilePage/_hr.vue
@@ -3,12 +3,12 @@
     <thead>
       <tr>
         <th>Documents</th>
-        <th>{{hoge}}</th>
+        <th>{{classid}}</th>
       </tr>
     </thead>
     <tbody>
       <tr v-for="document in documents">
-        <td>{{document.name}}</td>
+        <td>{{document.docid}}</td>
         <td>
           <div class="btn btn-danger" v-on:click="seeDocument(document.id)">See</div>
         </td>
@@ -17,18 +17,39 @@
   </table>
 </template>
 <script>
+import axios from 'axios'
+
 export default{
   data(){
     return{
-      hoge:'hello,world!',
+      classid:'',
       documents:[
-        {id:1,name:'sample01'},
-        {id:2,name:'sample02'},
+        {id:1,docid:'sample01'},
+        {id:2,docid:'sample02'},
       ],
       newDocument:'',
     }
   },
-
+  //リスト更新
+  asyncData ({ params, error }) {
+    //return axios.get('http://' +process.env.mainUrl + '/api/classes-list').then(res =>{
+    return axios.get('http://'+ process.env.mainUrl + '/api/class-docs?classid='+params.hr).then(res =>{
+      let docList = res.data
+      return {
+        classid:params.hr/*documentList*/,
+        documents: docList
+      }
+    })
+  },
+  methods:{
+    //ドキュメント閲覧
+    seeDocument(document_id){
+      this.documents.forEach(function (document) {
+        if (document.id === document_id) {
+        }
+      })
+    },
+  },
 }
 </script>
 <style>
