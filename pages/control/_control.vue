c3777761b3803cad29e4e46091b37c416530e1b7
siz <91zyutz@gmail.com>
2018-08-22 04:32:36 +0900

    [add]Uploadするclassidを指定するModalの追加　細かいリファクタ

--- pages/control/_control.vue
+++ pages/control/_control.vue
@@ -1,7 +1,7 @@
 <template>
     <section id="wrapper">
       <ControlSelecter :classes="classes"/>
-      <Control id="upload" :classid="classid"/>
+      <Control id="upload" :classid="classid" :classes="classes"/>
     </section>
 </template>
 <script>
@@ -23,7 +23,7 @@ export default {
       var eflag = true
       classlist.forEach(c => {
         if(String(c.classid) === params.control) eflag = false
-      });
+      })
       if(eflag) throw new URIError("URIちがうよ");
 
       return {
