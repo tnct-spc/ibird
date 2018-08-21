fd29a15c0791c61afdfed471e653042a3ef3b0cf
SuzukiSoichiro <s18094@tokyo.kosen-ac.jp>
2018-08-21 21:28:05 +0900

    [add]location.reload()の追加

---
+++ components/RainCloud.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <img class='weather_forecast' :src="url" alt=''>
+    <button @click="reload()">button2</button>
+  </div>
+</template>
+<script>
+export default {
+  data:function(){
+    return{
+      url: 'http://localhost:3000/api/yolp?lat=' + lat + '&lon=' + lon + '&z=' + z + '&width=' + width + '&height=' + height
+      lat:
+      lon:
+      z:
+      width:
+      height:
+    }
+  },
+  methods: {
+    reload:function() {
+      location.reload()
+    }
+  }
+}
+</script>
+<style scoped>
+</style>
