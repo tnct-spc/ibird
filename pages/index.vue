244991c8c064b3f33cba15d22e82fd306eb3940f
SuzukiSoichiro <s18094@tokyo.kosen-ac.jp>
2018-08-21 21:31:01 +0900

    [add]RainCloud.vueを書き加えた

--- pages/index.vue
+++ pages/index.vue
@@ -2,16 +2,20 @@
   <section class="container">
     <BulletinBoard />
     <Alert />
+    <RainCloud />
   </section>
 </template>
 
 <script>
 import BulletinBoard from '~/components/BulletinBoard.vue'
 import Alert from '~/components/Alert.vue'
+import RainCloud from '~/components/RainCloud.vue'
+
 export default {
   components: {
     BulletinBoard,
-    Alert
+    Alert,
+    RainCloud
   },
 }
 </script>
