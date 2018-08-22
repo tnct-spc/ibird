86fef462c2ad16957c3225835bbeb39f63bf4979
OguraNozomu <s17040@tokyo.kosen-ac.jp>
2018-08-22 19:56:13 +0900

    [update]いろいろ

--- pages/mobilepage/_hr.vue
+++ pages/mobilepage/_hr.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home" v-if="display">
-    <table id="Documents-index" class="table table-striped">
+    <table class="table table-striped">
       <thead>
         <tr>
           <th>Documents</th>
@@ -8,18 +8,23 @@
         </tr>
       </thead>
       <tbody>
-        <tr v-for="document in documents">
+        <tr v-for="(document) in documents">
+          <td>{{documents.indexOf(document)}}</td>
           <td>{{document.docid}}</td>
           <td>
-            <div class="btn btn-danger" v-on:click="seeDocument()">See</div>
+            <button class="btn btn-primary" v-on:click="seeDocument(document.docid)">See</button>
           </td>
         </tr>
       </tbody>
     </table>
   </div>
   <div v-else>
-    <h1>hoge</h1>
-    <img src=""></img>
+    <h1>seeDocument</h1>
+    <p>{{classid}}</p>
+    <img width="100%"v-bind:src="'/jpg/'+img+'.jpg'"></img>
+    <div align="right" >
+      <button class="btn btn-primary text-right" v-on:click="display=true">back</button>
+    </div>
   </div>
 </template>
 <script>
@@ -29,21 +34,26 @@ export default{
   data(){
     return{
       display:true,
+      // img:null,
+      img:'20180403',
+      classid:null
     }
   },
   //リスト更新
   asyncData ({ params, error }) {
+    let newId
     return axios.get('http://'+ process.env.mainUrl + '/api/class-docs?classid='+params.hr).then(res =>{
       let docList = res.data
       return {
-        classid:params.hr,
         documents: docList
       }
     })
   },
   methods:{
     //ドキュメント閲覧
-    seeDocument(){
+    seeDocument:function(document_docid){
+      this.classid=document.docid
+      this.display=false
     },
   },
 }
