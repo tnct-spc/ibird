ae300d010f92aee70a6be71ed123b7fdceacc5f4
ynakano1127 <ynakano1127@gmail.com>
2018-08-29 04:17:00 +0900

    [add]QRコードを作成

---
+++ components/QR.vue
@@ -0,0 +1,26 @@
+<template>
+  <svg ref="qrcode" v-html="svgCode" height="100" width="100"></svg>
+</template>
+<script>
+import QRCode from 'qrcode'
+export default {
+  props: {
+    url: String
+  },
+  data: () => {
+    return {svgCode: ''}
+  },
+  mounted () {
+    QRCode.toString(this.url, {
+      type: 'svg',
+      color: {
+        light: '#0000'
+      }
+    }, (error, string) => {
+      if (error) console.log(error)
+      this.svgCode = string
+    })
+  }
+}
+</script>
+<style></style>
