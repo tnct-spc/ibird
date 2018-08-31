76f2a255d9c1c1af468d410a5f0c7240d1740cdb
ynakano1127 <ynakano1127@gmail.com>
2018-08-31 21:20:07 +0900

    [add]tagを追加

--- components/strict/Strict.vue
+++ components/strict/Strict.vue
@@ -1,11 +1,27 @@
 <template>
   <section>
-      <p>hogehoge</p>
+    <h1>掲示板設定ページ</h1>
+    <div>
+        <span>全学年数 : </span>
+        <input v-model.number="yearsNumber" type="number">
+    </div>
+    <div>
+        <span>学科 : </span>
+        <div  v-for="(course, index) in courses" :key="index">
+            {{course}}
+        </div>
+    </div>
   </section>
 </template>
 
 <script>
 export default {
+    data() {
+        return {
+            yearsNumber: 5,
+            courses: ['J','M','C']
+        }
+    }
 }
 </script>
 <style>
