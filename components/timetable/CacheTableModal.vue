d0a5e66e2f81d1e8247c0c1e5bc06b5c9e7c6bb9
KitsuKitsu <akira1410.k@gmail.com>
2018-09-04 06:01:38 +0900

    [add]とりあえずModalを表示させた

---
+++ components/timetable/CacheTableModal.vue
@@ -0,0 +1,53 @@
+<template>
+  <transition name="cacheTableModal">
+    <div class="overlay" @click="$emit('close')">
+      <div class="panel" @click.stop>
+        <h3>CacheTableModal</h3>
+        <button @click="$emit('close')">閉じる</button>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+<style lang='scss' scoped>
+.overlay {
+  background: rgba(0, 0, 0, .8);
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  left: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 900;
+  transition: all .5s ease;
+}
+ 
+.panel {
+  width: 300px;
+  height: 200px;
+  background: #fff;
+  padding: 20px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  margin-left: -150px;
+  margin-top: -100px;
+  transition: all .3s ease;
+}
+.cacheTableModal-enter,
+.cacheTableModal-active {
+  opacity: 0;
+}
+ 
+.cacheTableModal-enter .panel,
+.cacheTableModal-leave-active .panel{
+  top: -200px;
+}
+</style>
+
---
+++ pages/timetable/index.vue
@@ -0,0 +1,24 @@
+<template>
+<div id='app'>
+  <button @click="showModal = true">モーダル表示</button>
+  <modal v-if="showModal" @close="showModal = false">
+    <CacheTableModal/>
+  </modal>
+</div>
+</template>
+<script>
+import CacheTableModal from '~/components/timetable/CacheTableModal.vue'
+export default{
+  name: 'app',
+  data() {
+    return {
+      showModal: false
+    }
+  },
+  components: {
+    CacheTableModal
+  }
+}
+</script>
+<style scoped>
+</style>
