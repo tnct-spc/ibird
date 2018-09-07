e01239d8f3d30163c30b745470d6a472b83772eb
YutoTamura <s17130@tokyo.kosen-ac.jp>
2018-09-07 17:39:09 +0900

    [update] リファクタリング

--- server/api/classes.js
+++ server/api/classes.js
@@ -185,64 +185,57 @@ router.get('/class-docs-mobile', (req, res, next) => {
     })
 })
 const sortDocs = (classid) => {
-    var makeRandom = true
-    classes.findById(classid)
-      .then(result => {
-          makeRandom = result.randomSort
-      }).catch(err => {
-          console.log(err)
-          return 400
-      })
-      console.log(makeRandom)
-    //並べる場所,今はてきとう
-    const cleanXYS = [
-      {x:200,y:400},
-      {x:2200,y:400},
-      {x:4200,y:400},
-      {x:6200,y:400},
-      {x:8200,y:400},
-      {x:200,y:5400},
-      {x:2200,y:5400},
-      {x:4200,y:5400},
-      {x:6200,y:5400},
-      {x:8200,y:5400},
-    ]
-    docList(classid).then(list =>{
-        //list並べる順番にsortする処理
-        const now = new Date()
-        var temp = list
-        temp.sort((a, b) => {
-          if (a.endTime < b.endTime) return -1;
-          if (a.endTime > b.endTime) return 1;
-          if (a.docid < b.docid) return -1;
-          if (a.docid > b.docid) return 1;
-          return 0
-        })
-        var plis = []
-        var lis = []
-        temp.forEach(v => {
-            if (v.priority === 1) {
-                plis.push(v)
-            } else {
-                lis.push(v)
-            }
-        })
-        console.log(sortedList)
-        var sortedList = plis.concat(lis)
-
-        for (let i=0; i<sortedList.length; i++) {
-          sortedList[i].x = cleanXYS[i].x
-          sortedList[i].y = cleanXYS[i].y
-          if (makeRandom){
-            sortedList[i].x += Math.random()*250 - 125
-            sortedList[i].y += Math.random()*250 - 125
-          }
-          sortedList[i].save()
-        }
-        const c = new W3cwebsocket('ws://localhost:3000/ws/refresh')
-        c.onopen = () => c.send('{}')
-      })
+  var makeRandom = ''
+  classes.findById(classid)
+    .then(result => {
+      makeRandom = result.randomSort
+    }).catch(err => {
+      console.log(err)
+      return 400
+    })
+  // 並べる場所,今はてきとう
+  const cleanXYS = [
+    {x: 200, y: 400},
+    {x: 2200, y: 400},
+    {x: 4200, y: 400},
+    {x: 6200, y: 400},
+    {x: 8200, y: 400},
+    {x: 200, y: 5400},
+    {x: 2200, y: 5400},
+    {x: 4200, y: 5400},
+    {x: 6200, y: 5400},
+    {x: 8200, y: 5400}
+  ]
+  docList(classid).then(list => {
+    // list並べる順番にsortする処理
+    let temp = list
+    temp.sort((a, b) => {
+      if (a.endTime < b.endTime) return -1
+      if (a.endTime > b.endTime) return 1
+      if (a.docid < b.docid) return -1
+      if (a.docid > b.docid) return 1
+      return 0
+    })
+    let plis = []
+    let lis = []
+    temp.forEach(v => {
+      if (v.priority === 1) plis.push(v)
+      else lis.push(v)
+    })
+    let sortedList = plis.concat(lis)
+    for (let i = 0; i < sortedList.length; i++) {
+      sortedList[i].x = cleanXYS[i].x
+      sortedList[i].y = cleanXYS[i].y
+      if (makeRandom) {
+        sortedList[i].x += Math.random()*250 - 125
+        sortedList[i].y += Math.random()*250 - 125
+      }
+      sortedList[i].save()
     }
+    const c = new W3cwebsocket('ws://localhost:3000/ws/refresh')
+    c.onopen = () => c.send('{}')
+  })
+}
 router.put('/sort-docs', (req, res, next) => {
     const classid = req.body.classid
     sortDocs(classid)
