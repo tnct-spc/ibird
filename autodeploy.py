be9241658d5a59e169417882b778cee3712e54e3
YutoTamura <s17130@tokyo.kosen-ac.jp>
2018-09-09 19:46:46 +0900

    [fix]

--- autodeploy.py
+++ autodeploy.py
@@ -1,19 +1,22 @@
 from flask import Flask, abort, request 
-import json
 import subprocess
+from datetime import datetime
 
 app = Flask(__name__)
-
 
 @app.route('/', methods=['POST']) 
 def foo():
     if not request.json:
         abort(400)
     if request.json["ref"]=="refs/heads/release":
-        subprocess.check_output('git pull origin release')
-        subprocess.check_output('docker-compose down')
-        subprocess.check_output('docker-compose up -d')
-    return 200
+        print(request.json)
+        print('\n'*4)
+        print("updated")
+        datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+        subprocess.run(['git','pull'])
+        subprocess.run(['docker-compose','down'])
+        subprocess.run(['docker-compose','up','-d'])
+    return '200'
 
 
 if __name__ == '__main__':
