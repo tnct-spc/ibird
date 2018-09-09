681931d17cc5cc3a273886b538585d04309d7d7c
ynakano1127 <ynakano1127@gmail.com>
2018-09-09 19:45:14 +0900

    [add]行くせんを変えた

--- components/timetable/TimetableParent.vue
+++ components/timetable/TimetableParent.vue
@@ -1,7 +1,7 @@
 <template>
   <section>
-    <Timetable :holidayData="holidayData" :weekdaysData="weekdaysData" :weekenddaysData="weekenddaysData"/>
-    <Timetable :holidayData="holidayData" :weekdaysData="weekdaysData" :weekenddaysData="weekenddaysData"/>
+    <Timetable :holidayData="holidayData1" :weekdaysData="weekdaysData1" :weekenddaysData="weekenddaysData1"/>
+    <Timetable :holidayData="holidayData2" :weekdaysData="weekdaysData2" :weekenddaysData="weekenddaysData2"/>
   </section>
 </template>
 <script>
@@ -10,9 +10,12 @@ import Timetable from '~/components/timetable/Timetable.vue'
 export default {
   data:function(){
     return{
-        holidayData: require("~/.timetable/holidays_22900_1532075792594.json"),
-        weekdaysData: require("~/.timetable/weekdays_22900_1532075866616.json"),
-        weekenddaysData: require("~/.timetable/weekenddays_22900_1532076033115.json")
+        holidayData1: require("~/.timetable/holidays_22900_1532075792594.json"),
+        weekdaysData1: require("~/.timetable/weekdays_22900_1532075866616.json"),
+        weekenddaysData1: require("~/.timetable/weekenddays_22900_1532076033115.json"),
+        holidayData2: require("~/.timetable/holidays_22900_1532075748963.json"),
+        weekdaysData2: require("~/.timetable/weekdays_22900_1532076326046.json"),
+        weekenddaysData2: require("~/.timetable/weekenddays_22900_1532076061582.json")
     }
   },
   components: {
