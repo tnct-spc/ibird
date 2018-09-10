2ec489788c9bc81c60a23ffe27617d9f1159c3ea
KitsuKitsu <akira1410.k@gmail.com>
2018-09-10 17:13:36 +0900

    [fix]自動で更新するようにした

--- components/RainCloud.vue
+++ components/RainCloud.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
     <img v-bind:src="weatherMap">
-    <button @click="reload()">reload</button>
   </div>
 </template>
 <script>
@@ -12,19 +11,21 @@ export default {
       weatherMap: null
       }
   },
+   mounted () {
+    this.reload()
+    setInterval(() => {
+      this.reload()
+    }, 120000)
+  },
   methods: {
-    reload: async function () {
+    reload: async function() {
+      var now = await moment().format('YYYYMMHHmm')
       const lat = '35.632789'
       const lon = '139.294293'
       const z = '15'
       var width = 'undefind'
       var height = 'undefind'
-      var now = await this.getTime()
       this.weatherMap = 'https://map.yahooapis.jp/map/V1/static?appid=dj0zaiZpPXVZaDlrczVieXNFYSZzPWNvbnN1bWVyc2VjcmV0Jng9ZGE-&lat=' + lat + '&lon=' + lon + '&z=' + z + '&width=' + width + '&height=' + height + '&mode=map&overlay=type:rainfall|date:' + now + '|datelabel:on&output=jpeg'
-    },
-    getTime: function () {
-      var now = moment()
-      return now.format('YYYYMMHHmm')
     }
   }
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,16 @@
 <template>
   <section class="container">
-    <BulletinBoard />
-    <Alert />
     <RainCloud />
   </section>
 </template>
 
 <script>
-import BulletinBoard from '~/components/BulletinBoard.vue'
-import Alert from '~/components/Alert.vue'
+//import BulletinBoard from '~/components/BulletinBoard.vue'
+//import Alert from '~/components/Alert.vue'
 import RainCloud from '~/components/RainCloud.vue'
 
 export default {
   components: {
-    BulletinBoard,
-    Alert,
     RainCloud
   },
 }
