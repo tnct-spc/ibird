5e6c8ac014cc2a7f1d77cb28eac8931b56040c49
YutoTamura <s17130@tokyo.kosen-ac.jp>
2018-09-25 18:02:51 +0900

    [hotfix]yearsAndCourses的なtypoしていた

--- server/api/classes/index.js
+++ server/api/classes/index.js
@@ -2,12 +2,12 @@ import { Router } from 'express'
 
 import classid from './class-id'
 import classes from './classes'
-import yearAndCourses from './year-and-courses'
+import yearsAndCourses from './years-and-courses'
 
 const router = Router()
 
 router.use('/class-id', classid)
 router.use('/classes', classes)
-router.use('/year-and-courses', yearAndCourses)
+router.use('/years-and-courses', yearsAndCourses)
 
 export default router
