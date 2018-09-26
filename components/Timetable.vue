010caebb1d74a8015f8b72cdfe4ef857e369c8da
Kuritain <s17068@tokyo.kosen-ac.jp>
2018-09-26 16:34:06 +0900

    [ADD]とりあえず祝祭日判定だけ加えた

--- components/Timetable.vue
+++ components/Timetable.vue
@@ -1,18 +1,17 @@
 <template>
-   <div class = "timetable">
-      <h1>次の電車</h1>
-      <p v-if = "object !== undefined && holidays !== undefined" class = "lineinfo">{{object.station,object.line,object.going}}</p>
-       <p v-if = "object !== undefined && holidays !== undefined" class = "traininfo">{{set.min,set.kind,set.going}}</p>
-      <p class = "nowtime">現在時刻 {{ hour }}：{{ minutes }}</p>
+   <div>
+    <b-card :header="stationData.station + ' ' + stationData.line + ' ' + stationData.direction" no-body>
+        <b-list-group flush>
+            <b-list-group-item v-for="(time, index) in nextTimes(2)" :key="index">
+                {{time.kind}} {{time.going}}行き {{time.hour}} : {{time.min}}
+            </b-list-group-item>
+        </b-list-group>
+    </b-card>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
-const holidayData = require("../.timetable/holidays_22900_1532075748963.json")
-const weekdaysData = require("../.timetable/weekdays_22900_1532076326046.json")
-const weekenddaysData = require("../.timetable/weekenddays_22900_1532076061582.json")
-
 export default {
     data : function(){
         return {
@@ -23,46 +22,31 @@ export default {
             minute : new Date().getMinutes(),
             dayOfWeek : new Date().getDay(),
             holidays : [[1,1],[2,11],[4,29],[5,3],[5,4],[5,5],[8,11],[11,3],[11,23],[12,23],[3,this.VerinalEquinox(this.year)],[9,this.AutumnalEquinox(this.year)]],
-            results : [],
         }
     },
+    props: {
+        "holidayData": Object,
+        "weekdaysData": Object,
+        "weekenddaysData": Object,
+    },
     created : function(){
-        console.log(holidayData)
-        // this.timer();
+        console.log(this.stationData);
+        this.timer();
     },
     computed : {
-        object : function() {
+        stationData : function() {
             if(this.dayOfWeek == 6){
-                return weekenddaysData;
-            }else if(this.dayOfWeek == 0 ||this.holidays.indexOf([this.month,this.day]) != -1 || (this.month == 1 && this.dayOfWeek == 1 && 7 < this.day && this.day < 15)
+                return this.weekenddaysData
+            }else if(this.dayOfWeek == 0
+            ||this.holidays.indexOf([this.month,this.day]) != -1 || (this.month == 1 && this.dayOfWeek == 1 && 7 < this.day && this.day < 15)
             || (this.month == 7 && this.dayOfWeek == 1 && 14 < this.day && this.day < 22)
             || (this.month == 9 && this.dayOfWeek == 1 && 14 < this.day && this.day < 22)
             || (this.month == 10 && this.dayOfWeek == 1 && 7 < this.day && this.day < 15)
-            || (this.SubstituteHoliday() == true)){
-                return holidayData;
-            } else {
-                return weekdaysData;
-            }
-        },
-        set : function(){
-            var i = 0;
-            var make = this.object.timetable[this.hour];
-                while(false){
-                    if (make[i].min >= this.minute){
-                        break;
-                    }else if(make.length >= i){
-                        make = this.object.timetable[this.hour + 1];
-                    }else{
-                        i++;
-                    }
-                }
-                return make[i];
-            },
-        minutes : function(){
-            if(new Date().getMinutes() < 10){
-                return 0 + new Date().getMinutes();
+            || (this.SubstituteHoliday() == true)
+            ){
+                return this.holidayData
             }else{
-                return new Date().getMinutes();
+                return this.weekdaysData
             }
         },
     },
@@ -102,13 +86,52 @@ export default {
                 }
             }
         },
+        nextTimes : function(elementNumber){ //何個の要素かを引数として取る
+            const times = []
+            var buff = null
+            for(var i = 0; i< elementNumber; i++){
+                console.log(i)
+                if(!buff){
+                    const firstTime = this.getNextTime(this.hour, this.minute)
+                    times.push(firstTime)
+                    buff = firstTime
+                }else{
+                    const time = this.getNextTime(buff.hour, buff.min)
+                    times.push(time)
+                    buff = time
+                }
+            }
+            return times
+        },
+        getNextTime : function(nHour, nMinute){ //現在の時間と分が引数(0≦nHour≦24, 0≦nMinute≦59)
+            nHour = nHour==0 ? 24 : nHour
+            const times = this.stationData.timetable[nHour]
+            for (var i = 0; i < times.length; i++) {
+                if(times[i].min > nMinute){
+                    times[i].hour = nHour
+                    return times[i]
+                }
+            }
+            var limitter = 0 //無限ループをしないため
+            while(limitter<1000){
+                nHour = nHour==24 ? 1 : nHour+1
+                const times = this.stationData.timetable[nHour]
+                if(times.length!==0){
+                    times[0].hour = nHour
+                    return times[0]
+                }
+                limitter++
+            }
+            return null
+        },
         timer : function(){
-            this.year = new Date().getFullYear();
-            this.month = new Date().getMonth() + 1;
-            this.day = new Date().getDate();
             this.hour = new Date().getHours();
             this.minute = new Date().getMinutes();
             this.dayOfWeek = new Date().getDay();
+            this.year = new Date().getFullYear();
+            this.month = new Date().getMonth() + 1;
+            this.day = new Date().getDate();
+            this.holidays = [[1,1],[2,11],[4,29],[5,3],[5,4],[5,5],[8,11],[11,3],[11,23],[12,23],[3,this.VerinalEquinox(this.year)],[9,this.AutumnalEquinox(this.year)]];
             setTimeout(this.timer,1000);
         }
     },
@@ -116,29 +139,4 @@ export default {
 </script>
 
 <style scoped>
-.timetable{
-    left : 0;
-    bottom : 0; 
-    height : 40%;
-    width : 40%;
-    background-color : olivedrab;
-    border : outset 1em forestgreen;
-}
-
-h1{
-    color : white
-  
-}
-.stationinfo{
-    margin : 1em;
-}
-
-.traininfo{
-    margin : 1em;
-}
-
-.nowtime{
-    color : white;
-
-}
 </style>
