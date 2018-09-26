e1aae6e14d5f120416581743d52ffa8281e1a6cc
ynakano1127 <ynakano1127@gmail.com>
2018-09-26 11:15:47 +0900

    [fix]lint --fix

--- server/migrations/20180817111740-changeName.js
+++ server/migrations/20180817111740-changeName.js
@@ -1,16 +1,16 @@
-'use strict';
+'use strict'
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return Promise.all([
-        queryInterface.renameColumn('documents', 'timeLimit', 'endTime'),
-        queryInterface.renameColumn('documents', 'scheduledDate', 'startTime')
+      queryInterface.renameColumn('documents', 'timeLimit', 'endTime'),
+      queryInterface.renameColumn('documents', 'scheduledDate', 'startTime')
     ])
   },
   down: (queryInterface, Sequelize) => {
     return Promise.all([
-        queryInterface.renameColumn('documents', 'endTime', 'timeLimit'),
-        queryInterface.renameColumn('documents', 'startTime', 'scheduledDate')
+      queryInterface.renameColumn('documents', 'endTime', 'timeLimit'),
+      queryInterface.renameColumn('documents', 'startTime', 'scheduledDate')
     ])
   }
-};
+}
--- server/websocket/alert.js
+++ server/websocket/alert.js
@@ -5,15 +5,15 @@ const router = Router()
 expressWs(router)
 
 var receivers = []
-router.ws('/alert',function(ws, req){
-  const idx = receivers.push(ws)-1
+router.ws('/alert', function (ws, req) {
+  const idx = receivers.push(ws) - 1
   ws.on('message', msg => {
-    receivers.forEach((receiver,receiverId)=>{
-      if(receiverId!==idx){receiver.send(msg)}
+    receivers.forEach((receiver, receiverId) => {
+      if (receiverId !== idx) { receiver.send(msg) }
     })
   })
-  ws.on('close', ()=>{
-    receivers = receivers.filter( v => v!==ws )
+  ws.on('close', () => {
+    receivers = receivers.filter(v => v !== ws)
   })
 })
 
