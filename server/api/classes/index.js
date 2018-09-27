2cdd64f3e6f1799940734d739f0b467904db783a
YutoTamura <s17130@tokyo.kosen-ac.jp>
2018-09-27 10:00:54 +0900

    [add]api

--- server/api/classes/index.js
+++ server/api/classes/index.js
@@ -3,11 +3,13 @@ import { Router } from 'express'
 import classid from './class-id'
 import classes from './classes'
 import yearsAndCourses from './years-and-courses'
+import classApi from './class'
 
 const router = Router()
 
 router.use('/class-id', classid)
 router.use('/classes', classes)
 router.use('/years-and-courses', yearsAndCourses)
+router.use('/class', classApi)
 
 export default router
