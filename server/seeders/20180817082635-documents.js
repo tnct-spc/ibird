25dad2ff3b31cc7c2a55cab532d2fd653f81b808
YutoTamura <s17130@tokyo.kosen-ac.jp>
2018-09-27 19:41:35 +0900

    [add]seedデータの画像を追加

--- server/seeders/20180817082635-documents.js
+++ server/seeders/20180817082635-documents.js
@@ -8,19 +8,6 @@ module.exports = {
         docid: '1',
         x: 0,
         y: 0,
-        sizeX: 2483,
-        sizeY: 3513,
-        priority: 1,
-        endTime: new Date(2020, 1, 1),
-        startTime: new Date(2001, 1, 1),
-        title: 'タイトルテスト',
-        openMobile: true
-      },
-      {
-        classid: 1,
-        docid: '2',
-        x: 0,
-        y: 0,
         sizeX: 4000,
         sizeY: 2250,
         priority: 1,
@@ -31,7 +18,7 @@ module.exports = {
       },
       {
         classid: 1,
-        docid: '4',
+        docid: '2',
         x: 0,
         y: 0,
         sizeX: 3513,
