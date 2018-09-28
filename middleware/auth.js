63f7cffe92d0105245188af14e2cca68b602ae66
YutoTamura <s17130@tokyo.kosen-ac.jp>
2018-09-28 20:39:26 +0900

    [add]ユーザーの権限ごとにページを見れなくした

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,24 @@
-export default function ({ store, redirect }) {
-  if (!store.state.authUser) {
-    return redirect('/login')
+export default function ({ route, store, redirect }) {
+  const path = route.path
+  console.log(path)
+  const user = store.state.authUser
+  let isRedirect = true
+  if (user) {
+    if (path.match(/control/)) {
+      if (user.control) {
+        isRedirect = false
+      }
+    } else if (path.match(/bb/)) {
+      if (user.bb) {
+        isRedirect = false
+      }
+    } else if (path.match(/mobilepage/)) {
+      if (user.mobile) {
+        isRedirect = false
+      }
+    }
+  }
+  if (isRedirect) {
+    return redirect('/login?got=' + path)
   }
 }
