56634f3c62364d03a37eb364904e341a5c134bc4
YutoTamura <s17130@tokyo.kosen-ac.jp>
2018-09-28 18:20:46 +0900

    [add]権限用のcolumnとdemouserのseedを追加

--- server/seeders/20180827112638-demouser.js
+++ server/seeders/20180827112638-demouser.js
@@ -2,11 +2,48 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('users', [{
-      id: 1,
-      username: 'demouser',
-      hashedPassword: 'asdfghjkl:sha512:2000:695a37e87ad28e4e464514c4f1fa79379faa881c54b64f0f7eb56a8f2b820bf109d5dd5b1763d9adc83b784ff5d89de1571f4b7cbb3903d49fcc12bff8ee7cea'
-    }], {})
+    return queryInterface.bulkInsert('users',
+      [
+        {
+          id: 1,
+          username: 'demouser',
+          hashedPassword: 'asdfghjkl:sha512:2000:695a37e87ad28e4e464514c4f1fa79379faa881c54b64f0f7eb56a8f2b820bf109d5dd5b1763d9adc83b784ff5d89de1571f4b7cbb3903d49fcc12bff8ee7cea',
+          allClass: true,
+          openMobile: true,
+          openControl: true,
+          openBB: true
+        },
+        {
+          id: 2,
+          username: 'demAdminUser',
+          hashedPassword: 'asdfghjkl:sha512:2000:695a37e87ad28e4e464514c4f1fa79379faa881c54b64f0f7eb56a8f2b820bf109d5dd5b1763d9adc83b784ff5d89de1571f4b7cbb3903d49fcc12bff8ee7cea',
+          allClass: true,
+          openMobile: true,
+          openControl: true,
+          openBB: false
+        },
+        {
+          id: 3,
+          username: 'demostudent',
+          hashedPassword: 'asdfghjkl:sha512:2000:695a37e87ad28e4e464514c4f1fa79379faa881c54b64f0f7eb56a8f2b820bf109d5dd5b1763d9adc83b784ff5d89de1571f4b7cbb3903d49fcc12bff8ee7cea',
+          allClass: false,
+          authorityClasses: [1],
+          openMobile: true,
+          openControl: false,
+          openBB: false
+        },
+        {
+          id: 4,
+          username: 'demobb',
+          hashedPassword: 'asdfghjkl:sha512:2000:695a37e87ad28e4e464514c4f1fa79379faa881c54b64f0f7eb56a8f2b820bf109d5dd5b1763d9adc83b784ff5d89de1571f4b7cbb3903d49fcc12bff8ee7cea',
+          allClass: false,
+          authorityClasses: [1],
+          openMobile: false,
+          openControl: false,
+          openBB: true
+        }
+      ]
+      , {})
   },
   down: (queryInterface, Sequelize) => {
     return queryInterface.bulkDelete('users', null, {})
