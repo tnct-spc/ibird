c360e766b1f8bb5d7143f2a6c30c3c939ce9b924
YutoTamura <s17130@tokyo.kosen-ac.jp>
2018-10-02 10:08:36 +0900

    [udate]years-and-courses API
    返すものをsortするようにした

--- server/api/classes/years-and-courses.js
+++ server/api/classes/years-and-courses.js
@@ -8,20 +8,31 @@ router.use(parser.urlencoded({ extended: false }))
 router.use(parser.json())
 
 router.get('/', (req, res, next) => {
-  let courseset = new Set()
+  let courses = []
   let yearset = new Set()
   classes.findAll()
     .then(result => {
-      result.forEach(e => {
-        courseset.add(e.year)
-        yearset.add(e.course)
+      let results = result
+      results.sort((a, b) => {
+        if (a.index > b.index) return 1
+        if (a.index < b.index) return -1
+        return 0
       })
-      const obj = {
-        years: Array.from(courseset.values()),
-        courses: Array.from(yearset.values())
-      }
-      console.log(obj)
-      res.json(obj)
+      results.forEach(result => {
+        if (courses.indexOf(result.course) === -1) {
+          courses.push(result.course)
+        }
+      })
+      result.forEach(e => {
+        yearset.add(e.year)
+      })
+      res.json({
+        courses: courses,
+        years: Array.from(yearset.values()).sort()
+      })
+    }).catch(err => {
+      console.log(err)
+      res.status(400).send('faild')
     })
 })
 
