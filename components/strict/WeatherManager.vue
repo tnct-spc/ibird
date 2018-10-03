a3b9fc5c02cc7f85e77878b3b7e1dc2c785e9f8b
SuzukiSoichiro <s18094@tokyo.kosen-ac.jp>
2018-10-03 21:40:28 +0900

    [add]レビューボタンを追加

--- components/strict/WeatherManager.vue
+++ components/strict/WeatherManager.vue
@@ -3,10 +3,18 @@
     <h1>天気情報</h1>
     <form>
         <div class="form-group">
-            <label>緯度</label>
-            <input class="form-control">
-            <label>経度</label>
-            <input class="form-control">
+            <div style="float: left;">
+                <label>緯度</label>
+                <input class="form-control" pattern="\d" required>
+                <br>
+                <label>経度</label>
+                <input class="form-control" pattern="\d" required>
+            </div>
+            <div style="float: right;">
+                <button style="width: 21em; margin: 1em;">レビュー</button>
+                <br>
+                <img width="360px" height="360px" style="margin-bottom: 1em;">
+            </div>
         </div>
     </form>
   </section>
@@ -16,5 +24,8 @@
 .container {
     padding-top: 2rem;
     padding-bottom: 2rem;
+}
+.form-control {
+    width: 36rem;
 }
 </style>
