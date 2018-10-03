0fa7ac5c0692d4c361e758f0c4f59cf0712c1e22
YutoTamura <s17130@tokyo.kosen-ac.jp>
2018-10-04 06:29:55 +0900

    [add]実装したはず

--- server/api/doc/docs.js
+++ server/api/doc/docs.js
@@ -3,8 +3,11 @@ import parser from 'body-parser'
 import models from '../../models'
 import sortDocs from '../../lib/sortdocs'
 import docList from '../../lib/doclist'
+import { w3cwebsocket } from 'websocket'
+const W3cwebsocket = w3cwebsocket
 
 const documents = models.documents
+const temporaryDatas = models.temporaryDatas
 const router = Router()
 router.use(parser.urlencoded({ extended: false }))
 router.use(parser.json())
@@ -17,35 +20,54 @@ router.get('/', (req, res, next) => {
 })
 
 router.post('/', (req, res, next) => {
-  const classids = req.body.classids
   const doc = req.body
-  const startTime = new Date(doc.startTime)
-  const endTime = new Date(doc.endTime)
-  let insertData = []
-  classids.forEach(id => {
-    insertData.push(
-      {
-        classid: id,
-        docid: doc.docid,
-        x: doc.x,
-        y: doc.y,
-        priority: doc.priority,
-        openMobile: doc.openMobile,
-        title: doc.title,
-        startTime: startTime,
-        endTime: endTime,
-        sizeX: doc.imgsize.width,
-        sizeY: doc.imgsize.height
+  const docid = req.body.docid
+  temporaryDatas.find({where: {
+    isActive: true,
+    NoCollisonKey: docid
+  }}).then(temporary => {
+    if (temporary === null) {
+      temporaryDatas.create({
+        NoCollisonKey: docid,
+        data: doc,
+        isActive: true
       })
+    } else {
+      temporary.isActive = false
+      temporary.save()
+      const classids = req.body.classids
+      const sizeX = temporary.data.width
+      const sizeY = temporary.data.height
+      const startTime = new Date(doc.startTime)
+      const endTime = new Date(doc.endTime)
+      let insertData = []
+      classids.forEach(id => {
+        insertData.push(
+          {
+            classid: id,
+            docid: docid,
+            x: doc.x,
+            y: doc.y,
+            priority: doc.priority,
+            openMobile: doc.openMobile,
+            title: doc.title,
+            startTime: startTime,
+            endTime: endTime,
+            sizeX: sizeX,
+            sizeY: sizeY
+          })
+      })
+      documents.bulkCreate(insertData)
+        .then(result => {
+          res.sendStatus(200)
+          classids.forEach(e => { sortDocs(e) })
+        }).catch(err => {
+          console.log(err)
+          res.sendStatus(400)
+        })
+    }
   })
-  documents.bulkCreate(insertData)
-    .then(result => {
-      res.sendStatus(200)
-      classids.forEach(e => { sortDocs(e) })
-    }).catch(err => {
-      console.log(err)
-      res.sendStatus(400)
-    })
+
 })
 
 export default router
--- server/api/upload-file.js
+++ server/api/upload-file.js
@@ -3,14 +3,16 @@ import { Router } from 'express'
 import multer from 'multer'
 import Converter from 'office-convert'
 import child_process from 'child_process'
-import axios from 'axios'
 import { w3cwebsocket } from 'websocket'
 import sizeOf from "image-size";
+import axios from 'axios'
 const W3cwebsocket = w3cwebsocket
+import models from '../models'
 
 const router = Router()
 const converter = Converter.createConverter();
 const office_extensions = ['docx','doc','xls','xlsx','ppt','pptx']
+const temporaryDatas = models.temporaryDatas
 
 //拡張子(.で切って一番最後の要素)を返す関数
 const extension = (filename) => {
@@ -41,11 +43,11 @@ const upload = multer({ storage: multer.diskStorage({
     cb(null, '.document/')
   },
   filename: (req, file, cb) => {
-    cb(null, Date.now() + '.' + extension(file.originalname))
+    cb(null, req.body.docid + '.' + extension(file.originalname))
   }
 })})
 
-async function run(path){
+async function run(path, docid){
   //拡張子がofficeだったらconvertしてpathにpdfのpathを入れる
   if(office_extensions.indexOf(extension(path)) >= 0){
     const result = await officeToPDF(path)
@@ -54,16 +56,60 @@ async function run(path){
   var docid = pdfToJpg(path)
   docid = docid.slice(0, -4)
   const imgsize = sizeOf(`static/jpg/${docid}.jpg`);
-  return {docid: docid, imgsize: imgsize}
+  return imgsize
 }
 
 router.post('/upload-file', upload.single('file'), (req, res, next) => {
-  run(req.file.path).then((doc) =>{
-    console.log(doc.docid)
-    res.status(200).json(doc)
-  }).catch(e =>{
-    console.log(e)
-    res.sendStatus(400)
+  const docid = req.body.docid
+  run(req.file.path, docid).then((imgsize) =>{
+    temporaryDatas.find({where: {
+      isActive: true,
+      NoCollisonKey: docid
+    }}).then(temporary => {
+      if (temporary === null) {
+        temporaryDatas.create({
+          NoCollisonKey: docid,
+          data: imgsize,
+          isActive: true
+        })
+      } else {
+        temporary.isActive = false
+        temporary.save()
+        const doc = temporary.data
+        const sizeX = imgsize.width
+        const sizeY = imgsize.height
+        const classids = req.doc.classids
+        const startTime = new Date(doc.startTime)
+        const endTime = new Date(doc.endTime)
+        classids.forEach(id => {
+          insertData.push(
+            {
+              classid: id,
+              docid: docid,
+              x: doc.x,
+              y: doc.y,
+              priority: doc.priority,
+              openMobile: doc.openMobile,
+              title: doc.title,
+              startTime: startTime,
+              endTime: endTime,
+              sizeX: sizeX,
+              sizeY: sizeY
+            })
+        })
+        documents.bulkCreate(insertData)
+          .then(result => {
+            res.sendStatus(200)
+            classids.forEach(e => { sortDocs(e) })
+          }).catch(err => {
+            console.log(err)
+            res.sendStatus(400)
+          })
+        }
+    }).catch(e =>{
+      console.log(e)
+      res.sendStatus(400)
+    })
   })
 })
 
