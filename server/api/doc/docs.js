f1c6f213bb63f4711279dbaaac2e22a4709a3ff1
YutoTamura <s17130@tokyo.kosen-ac.jp>
2018-10-10 15:09:54 +0900

    [add] ドキュメントをまとめて消すAPI

--- server/api/doc/docs.js
+++ server/api/doc/docs.js
@@ -17,6 +17,21 @@ router.get('/', (req, res, next) => {
   })
 })
 
+router.delete('/', (req, res, next) => {
+  const docid = req.query.docid
+  let classids = new Set()
+  documents.findAll({where: {docid: docid}})
+    .then(docs => {
+      docs.forEach(doc => classids.add(doc.classid))
+      documents.destroy({where: {docid: docid}})
+      classids.forEach(classid => sortDocs(classid))
+      res.sendStatus(200)
+    }).catch(err => {
+      console.log(err)
+      res.sendStatus(400)
+    })
+})
+
 router.post('/', (req, res, next) => {
   const doc = req.body
   const docid = req.body.docid
