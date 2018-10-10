ca19c117fe92b311cc23dae6129da4ba309f069a
SuzukiSoichiro <s18094@tokyo.kosen-ac.jp>
2018-10-10 19:30:19 +0900

    [add]ボタンを押すと画像が表示されるようにした

--- components/strict/WeatherManager.vue
+++ components/strict/WeatherManager.vue
@@ -6,21 +6,32 @@
             <div class="row">
                 <div class="col">
                     <label>緯度</label>
-                    <input class="form-control" pattern="\d+" required>
+                    <input type="text" class="form-control" pattern="\d+" required>
                     <br>
                     <label>経度</label>
-                    <input class="form-control" pattern="\d+" required>
+                    <input type="text" class="form-control" pattern="\d+" required>
                 </div>
-                <div style="col">
-                    <input type="submit" value="レビュー" style="width: 360px; margin: 9px;">
+                <div class="col">
+                    <input @click="display = true" type="submit" value="レビュー" style="width: 360px; margin: 9px;">
                     <br>
-                    <img width="360px" height="360px" style="margin: 9px; margin-right: 18px;">
+                    <img v-if="display" src="https://map.yahooapis.jp/map/V1/static?appid=dj0zaiZpPXVZaDlrczVieXNFYSZzPWNvbnN1bWVyc2VjcmV0Jng9ZGE-&lat=35.632789&lon=139.294293&z=12&mode=map&overlay=type:rainfall|date:201810101820|datelabel:on&output=jpeg" width="360px" height="360px" style="margin: 9px; margin-right: 18px;">
                 </div>
             </div>
         </div>
     </form>
   </section>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            display: false
+        }
+    }
+}
+</script>
+
 
 <style scoped>
 .container {
@@ -31,3 +42,4 @@
     width: 36rem;
 }
 </style>
+
