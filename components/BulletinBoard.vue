3ac026172a8b3d4a500854bff05947156f83ba7c
siz <91zyutz@gmail.com>
2018-10-10 18:37:29 +0900

    [update]クラスが選択されていない時の表示

--- components/BulletinBoard.vue
+++ components/BulletinBoard.vue
@@ -3,6 +3,9 @@
     <div id="wrapper">
       <ViewPaper v-if="showPaper" @close="showPaper=false" :paper="papers[docid]" :docid="docid"/>
       <div id="content" ref="fieldElm">
+       <b-alert show>
+         <span style="font-size:50px;font-family: 'Sawarabi Mincho', sans-serif">{{noClassid}}</span>
+       </b-alert>
        <div :id=i v-for="(paper, i) in sortedPapers" @dblclick="viewPaper(paper.docid)">
         <Paper
           :key="i"
@@ -27,6 +30,13 @@ import { setInterval } from 'timers';
 const W3cwebsocket = w3cwebsocket
 
 export default {
+  head () {
+    return {
+      link: [
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Sawarabi+Mincho&amp;subset=japanese,latin-ext' }
+      ]
+    }
+  },
   props: {
     "classid": String,
   },
@@ -35,13 +45,15 @@ export default {
       showPaper:false,
       client: {},
       refreshClient: {},
-      docid:""
+      docid:"",
+      noClassid:""
     }
   },
   watch:{
     classid(){
       this.refreshClient = new w3cwebsocket(process.env.wsUrl + '/ws/refresh')
       this.refreshClient.onopen = () => this.refreshClient.send('')
+      this.noClassid=""
     }
   },
   created () {
@@ -68,6 +80,9 @@ export default {
       }
     }
     startWebsocketB()
+    this.$nextTick(() => {
+      if(!this.classid)this.noClassid="クラスを選択してください"
+    })
   },
   mounted() {
     window.addEventListener('resize', this.handleResize)
