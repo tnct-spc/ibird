2ed0fa6c4cbb43f8ba47836952329e9cb119aa4e
siz <91zyutz@gmail.com>
2018-10-19 21:17:15 +0900

    [add]clock

--- components/timetable/TimetableParent.vue
+++ components/timetable/TimetableParent.vue
@@ -1,5 +1,8 @@
 <template>
   <section>
+    <b-card class="text-center">
+      {{clockHour}}時:{{clockMinute}}分:{{clockSecond}}秒
+    </b-card>
     <Timetable :holidayData="holidayData1" :weekdaysData="weekdaysData1" :weekenddaysData="weekenddaysData1"/>
     <Timetable :holidayData="holidayData2" :weekdaysData="weekdaysData2" :weekenddaysData="weekenddaysData2"/>
   </section>
@@ -15,7 +18,21 @@ export default {
         weekenddaysData1: require("~/.timetable/weekenddays_22900_1532076033115.json"),
         holidayData2: require("~/.timetable/holidays_22900_1532075748963.json"),
         weekdaysData2: require("~/.timetable/weekdays_22900_1532076326046.json"),
-        weekenddaysData2: require("~/.timetable/weekenddays_22900_1532076061582.json")
+        weekenddaysData2: require("~/.timetable/weekenddays_22900_1532076061582.json"),
+        clockHour : new Date().getHours(),
+        clockMinute : new Date().getMinutes(),
+        clockSecond : new Date().getSeconds()
+    }
+  },
+  mounted(){
+    this.clock()
+  },
+  methods:{
+    clock : function(){
+        this.clockHour = new Date().getHours(),
+        this.clockMinute = new Date().getMinutes(),
+        this.clockSecond = new Date().getSeconds()
+        setTimeout(this.clock,100);
     }
   },
   components: {
