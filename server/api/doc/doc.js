24645867472ebb926f128f6ea6db78ec662f9995
YutoTamura <s17130@tokyo.kosen-ac.jp>
2018-10-20 12:32:52 +0900

    [add] クラス以外の情報を再設定するAPI

--- server/api/doc/doc.js
+++ server/api/doc/doc.js
@@ -11,7 +11,6 @@ router.use(parser.json())
 router.delete('/', (req, res, next) => {
   const classid = req.query.classid
   const docid = req.query.docid
-
   documents.destroy({
     where: {
       classid: classid,
@@ -26,32 +25,51 @@ router.delete('/', (req, res, next) => {
   })
 })
 
+router.get('/', (req, res, next) => {
+  const docid = req.query.docid
+  documents.findOne({where: {docid: docid}})
+    .then(doc => {
+      let endTime = doc.endTime.toISOString().substr(0, 10)
+      res.json({
+        priority: doc.priority,
+        endTime: endTime,
+        title: doc.title,
+        openMobile: doc.openMobile
+      })
+    })
+})
+
 router.put('/', (req, res, next) => {
   const classid = req.body.classid
   const docid = req.body.docid
   const x = req.body.x
   const y = req.body.y
-  documents.update(
-    {updatedAt: Date.now()},
-    {where: {classid: classid, docid: docid}}
-  ).then(result => {
-    if (x && y) {
-      documents.update(
-        {x: x, y: y},
-        {where: {classid: classid, docid: docid}}
-      ).then(result => {
-        res.sendStatus(200)
-      }).catch(err => {
-        console.log(err)
-        res.sendStatus(400)
-      })
-    } else {
+  const endTime = new Date(req.body.endTime)
+  const title = req.body.title
+  const openMobile = req.body.openMobile
+  const priority = req.body.priority
+  if (typeof classid !== 'undefined') {
+    documents.update(
+      {x: x, y: y},
+      {where: {classid: classid, docid: docid}}
+    ).then(result => {
       res.sendStatus(200)
-    }
-  }).catch(err => {
-    console.log(err)
-    res.sendStatus(400)
-  })
+    }).catch(() => {
+      res.sendStatus(400)
+    })
+  } else {
+    documents.update({
+      endTime: endTime,
+      title: title,
+      openMobile: openMobile,
+      priority: priority
+    }, {where: {docid: docid}
+    }).then(() => {
+      res.sendStatus(200)
+    }).catch(() => {
+      res.sendStatus(400)
+    })
+  }
 })
 
 export default router
