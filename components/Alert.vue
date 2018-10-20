534d53a44655b3dd18fb6535b3d8fa9865838ca1
siz <91zyutz@gmail.com>
2018-10-20 17:26:51 +0900

    [update]alertの仕様変更をbbに対応

--- components/Alert.vue
+++ components/Alert.vue
@@ -1,6 +1,6 @@
 <template>
   <section>
-    <div class="alert-div my-10"  v-show="this.message != null">
+    <div class="alert-div my-10"  v-show="this.message != '' ">
       <b-alert class=" b-alertc" show variant="danger">
         <p class="text-danger text-center my-auto alert-text">{{this.message}}</p>
       </b-alert>
@@ -16,36 +16,41 @@ const W3cwebsocket = w3cwebsocket
 export default {
   data () {
     return {
-      client: {},
-      message : null,
-      wsCheck : false
+      client: null,
+      endDate: null,
+      message : null
     }
   },
   created () {
    axios.get(process.env.httpUrl+'/api/alert')
    .then((res)=>{
      this.message = res.data.message
-     const endDate = new Date()
-     const unixTime = Math.floor( endDate.getTime() )
-     const deleteTime = res.data.endDate - unixTime
-     setTimeout(() => {
-       if(this.wsCheck===true)this.message=null
-     },deleteTime)
+     this.endDate = res.data.endDate
+     this.checkAlert()
    })
    const startWebsocket = () => {
       this.client = new W3cwebsocket(process.env.wsUrl+'/ws/alert')
-      this.client.onmessage=({data})=>{
-        this.message = JSON.parse(data).message
-        console.log(this.message)
-        setTimeout(() => {this.message=null},1000*60*30)
+      this.client.onmessage=()=>{
+        axios.get(process.env.httpUrl+'/api/alert')
+        .then((res)=>{
+          this.message = res.data.message
+          this.endDate = res.data.endDate
+        })
       }
-      this.wsCheck = true
       this.client.onclose=()=>{
         console.log('websocket disconnect ws/alert')
         setTimeout(() =>{startWebsocket()},1000)
       }
     }
     startWebsocket()
+    },
+    methods:{
+      checkAlert(){
+        const endDate = new Date()
+        const unixTime = Math.floor( endDate.getTime())
+        if(this.endDate <= unixTime)this.message="現在表示中のアラートはありません"
+        setTimeout(this.checkAlert(),1000*3)
+      }
     }
  }
 </script>
