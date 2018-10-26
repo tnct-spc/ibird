6fad4e31a6b85895ffe0040ce2bd221ea4d3ae91
siz <91zyutz@gmail.com>
2018-10-26 21:13:07 +0900

    [fix]リファクタ

--- components/timetable/TimetableParent.vue
+++ components/timetable/TimetableParent.vue
@@ -14,12 +14,6 @@ import axios from 'axios'
 export default {
   data:function(){
     return{
-        holidayData1: require("~/.timetable/holidays_22900_1532075792594.json"),
-        weekdaysData1: require("~/.timetable/weekdays_22900_1532075866616.json"),
-        weekenddaysData1: require("~/.timetable/weekenddays_22900_1532076033115.json"),
-        holidayData2: require("~/.timetable/holidays_22900_1532075748963.json"),
-        weekdaysData2: require("~/.timetable/weekdays_22900_1532076326046.json"),
-        weekenddaysData2: require("~/.timetable/weekenddays_22900_1532076061582.json"),
         clockHour : new Date().getHours(),
         clockMinute : new Date().getMinutes(),
         clockSecond : new Date().getSeconds(),
