d8c93f5588add17a17f17c64116f4cff427b0134
Yuto Tamura <yuto.tamur@gmail.com>
2019-07-09 20:50:44 +0900

    [fix] migrations file

--- server/migrations/20180807100524-yaer.js
+++ server/migrations/20180807100524-yaer.js
@@ -2,14 +2,18 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    queryInterface.addColumn('classes', 'year', Sequelize.TEXT)
-    queryInterface.addColumn('classes', 'course', Sequelize.TEXT)
-    queryInterface.removeColumn('classes', 'name')
+    return Promise.all([
+      queryInterface.addColumn('classes', 'year', Sequelize.TEXT),
+      queryInterface.addColumn('classes', 'course', Sequelize.TEXT),
+      queryInterface.removeColumn('classes', 'name')
+    ])
   },
 
   down: (queryInterface, Sequelize) => {
-    queryInterface.removeColumn('classes', 'year')
-    queryInterface.removeColumn('classes', 'course')
-    queryInterface.addColumn('classes', 'name', Sequelize.TEXT)
+    return Promise.all([
+      queryInterface.removeColumn('classes', 'year'),
+      queryInterface.removeColumn('classes', 'course'),
+      queryInterface.addColumn('classes', 'name', Sequelize.TEXT)
+    ])
   }
 }
